34d9dab3dcc889bf7877213e56dfcb2c7cfb17da
Mona Lisa <halvor98@outlook.com>
2018-07-04 19:21:01 +0200

    New template

--- manage.py
+++ manage.py
@@ -3,11 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.config")
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     try:
-        from configurations.management import execute_from_command_line
+        from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
@@ -20,5 +19,12 @@ if __name__ == "__main__":
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
             )
+
         raise
+
+    # This allows easy placement of apps within the interior
+    # api directory.
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, "api"))
+
     execute_from_command_line(sys.argv)
