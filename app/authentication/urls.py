3c82db596099ceefb8f362a069a302a0f0813dcf
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2018-07-20 20:08:09 +0200

    Added JWT based authentication using `djangorestframework_simplejwt`.

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -3,6 +3,16 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 
 # Register authentication viewsets here 
+from django.urls import re_path
 
 urlpatterns = router.urls
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    re_path(r'^token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(r'^token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- app/settings.py
+++ app/settings.py
@@ -58,7 +58,39 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+# JWT tokens
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=45),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': settings.SECRET_KEY,
+    'VERIFYING_KEY': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
+
 ROOT_URLCONF = 'app.urls'
+
 
 TEMPLATES = [
     {
