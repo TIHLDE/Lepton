20bc42068c48667c90dd0ed730314ddb213e0abf
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-07-29 18:54:33 +0200

    Added Image and ImageGallery to admin

--- app/content/admin.py
+++ app/content/admin.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.News)
-admin.site.register(models.EventList)
 admin.site.register(models.Event)
+admin.site.register(models.EventList)
+admin.site.register(models.News)
 admin.site.register(models.Poster)
+
+admin.site.register(models.Image)
+admin.site.register(models.ImageGallery)
+
 admin.site.register(models.Grid)
-admin.site.register(models.ManualGridItem)
 admin.site.register(models.ManualGrid)
+admin.site.register(models.ManualGridItem)
 admin.site.register(models.RecentFirstGrid)
