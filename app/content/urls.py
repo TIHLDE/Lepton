75831b422019d4a9cf14d9ef577d53b197f58f49
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-10-19 18:37:34 +0200

    Changed queryset in EventViewSet

--- app/content/urls.py
+++ app/content/urls.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter()
 # Register content viewpoints here
 router.register('items', ItemViewSet)
 router.register('news', NewsViewSet)
-router.register('events', EventViewSet)
+router.register('events', EventViewSet, base_name='event')
 router.register('eventlist', EventListViewSet)
 router.register('posters', PosterViewSet)
 router.register('grids', GridViewSet)
