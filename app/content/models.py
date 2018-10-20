6ce6ec1bcea9b90d80bf1346d85492051e250aa6
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-10-20 16:07:22 +0200

    Added category

--- app/content/models.py
+++ app/content/models.py
@@ -52,7 +52,12 @@ class Event(BaseModel, OptionalImage):
     )
     priority = models.IntegerField(default=0, choices=PRIORITIES, null=True)
 
-    category = models.ForeignKey(Category, related_name='category',blank=True, null=True, default=None, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='category',blank=True,
+                                    null=True, default=None,
+                                    on_delete=models.CASCADE, setter='set_category')
+
+    def set_category(self, value):
+        return value
 
     @property
     def expired(self):
