58cf26ba75bc0882caa041d0afeaac575445c65e
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-25 23:49:20 +0100

    fixed static path

--- app/settings.py
+++ app/settings.py
@@ -137,7 +137,7 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = [
