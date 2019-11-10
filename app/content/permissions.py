bf5786d3a31b6f1d5ede47db2848065ecb1ca731
zuimran <zuimran@stud.ntnu.no>
2019-11-10 17:05:47 +0100

    Make user get endpoints more useful and clean permission class

--- app/content/permissions.py
+++ app/content/permissions.py
@@ -12,93 +12,93 @@ VERIFY_URL = API_URL + '/verify'
 
 # Checks if the user is a member
 class IsMember(permissions.BasePermission):
-    message = 'You are not a member'
+	message = 'You are not a member'
 
-    def has_permission(self, request, view):
-        # Check if session-token is provided
-        token = request.META.get('HTTP_X_CSRF_TOKEN')
-        if token is None:
-            return False
+	def has_permission(self, request, view):
+		# Check if session-token is provided
+		user = get_user_info(request)
 
-        info = get_user_info(token)
+		if user is None:
+			return False
 
-        if info is None:
-            return False
+		request.info = user
+		return True
 
-        request.info = info
-        return True
+	def isDev(self, request):
+		user = get_user_info(request)
+		if user is None:
+			return False
+		user_id = user['uid'][0]
+		return User.objects.filter(user_id=user_id).filter(groups__name__in= ['DevKom','HS']).count() > 0
 
 
 # Checks if the user is in HS or Drift
 class IsDev(permissions.BasePermission):
-    message = 'You are not in DevKom'
+	message = 'You are not in DevKom'
 
-    def has_permission(self, request, view):
-        return check_group_permission(self, request, view, ['DevKom'])
+	def has_permission(self, request, view):
+		return check_group_permission(self, request, view, ['DevKom'])
 
 
 # Checks if the user is in HS or Drift
 class IsHS(permissions.BasePermission):
-    message = 'You are not in HS'
+	message = 'You are not in HS'
 
-    def has_permission(self, request, view):
-        return check_group_permission(self, request, view, ['HS', 'DevKom'])
+	def has_permission(self, request, view):
+		return check_group_permission(self, request, view, ['HS', 'DevKom'])
 
 
 # Checks if the user is in HS, Drift, or Promo
 class IsPromo(permissions.BasePermission):
-    message = 'You are not in Promo'
+	message = 'You are not in Promo'
 
-    def has_permission(self, request, view):
-        return check_group_permission(self, request, view, ['HS', 'DevKom' 'Promo'])
+	def has_permission(self, request, view):
+		return check_group_permission(self, request, view, ['HS', 'DevKom' 'Promo'])
 
 
 # Checks if the user is in HS, Drift, or NoK
 class IsNoK(permissions.BasePermission):
-    message = 'You are not in NoK'
+	message = 'You are not in NoK'
 
-    def has_permission(self, request, view):
-        return check_group_permission(self, request, view, ['HS', 'DevKom', 'NoK'])
+	def has_permission(self, request, view):
+		return check_group_permission(self, request, view, ['HS', 'DevKom', 'NoK'])
 
 
 # Checks if the user is in HS, Drift, or NoK
 class IsNoKorPromo(permissions.BasePermission):
-    message = 'You are not in NoK'
+	message = 'You are not in NoK'
 
-    def has_permission(self, request, view):
-        return check_group_permission(self, request, view, ['HS', 'DevKom', 'NoK', 'Promo'])
+	def has_permission(self, request, view):
+		return check_group_permission(self, request, view, ['HS', 'DevKom', 'NoK', 'Promo'])
 
 
 def check_group_permission(self, request, view, groups):
-    # Allow GET, HEAD or OPTIONS requests
-    if request.method in permissions.SAFE_METHODS:
-        return True
+	# Allow GET, HEAD or OPTIONS requests
+	if request.method in permissions.SAFE_METHODS:
+		return True
+	# Check if session-token is provided
+	user = get_user_info(request)
 
-    # Check if session-token is provided
-    token = request.META.get('HTTP_X_CSRF_TOKEN')
-    if token is None:
-        return permissions.IsAdminUser.has_permission(self, request, view) # Allow access if is Admin
+	if user is None:
+		return False
 
-    # Gets the user id
-    user = get_user_info(token)
+	request.info = user
+	user_id = user['uid'][0]
 
-    if user is None:
-        return False
-
-    user_id = user['uid'][0]
-
-    # Check if user with given id is connected to Groups
-    return User.objects.filter(user_id=user_id).filter(groups__name__in=groups).count() > 0
+	# Check if user with given id is connected to Groups
+	return User.objects.filter(user_id=user_id).filter(groups__name__in=groups).count() > 0
 
 
-def get_user_info(token):
-    # Get user ID from token
-    headers = {'X-CSRF-TOKEN': token}
-    r = requests.get(VERIFY_URL, headers=headers, verify=False)  # Send request to verify token
-    response = r.json()
+def get_user_info(request):
+	token = request.META.get('HTTP_X_CSRF_TOKEN')
+	if token is None:
+		return None
+	# Get user ID from token
+	headers = {'X-CSRF-TOKEN': token}
+	r = requests.get(VERIFY_URL, headers=headers, verify=False)  # Send request to verify token
+	response = r.json()
 
-    if r.status_code is not 200 or 'uid' not in response:
-        return None
+	if r.status_code is not 200 or 'uid' not in response:
+		return None
 
-    return response
-
+	return response
