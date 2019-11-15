6136b37e6c0ebf43a5b78f7ff4164ea00c677461
Eirik Steira <steiraeirik@gmail.com>
2019-11-15 15:09:08 +0100

    Modify create and update methods on UserEvent model

--- app/content/models/user_event.py
+++ app/content/models/user_event.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 
 from app.util.models import BaseModel
 from .user import User
@@ -18,31 +19,38 @@ class UserEvent(BaseModel):
         verbose_name_plural = 'User events'
 
     def save(self, *args, **kwargs):
-        if self.user_event_id:
+        """ Determines whether the object is being created or updated """
+        if not self.user_event_id:
             return self.create(*args, **kwargs)
         return self.update(*args, **kwargs)
 
-    def create(self, *args, **kwargs):
+    def create(self, *args, **kwargs): 
+        """
+        Validates model fields upon creation.
+        Determines whether user is on the waiting list or not when the instance is created
+        :raises ValidationError if the event is closed. If so, the object will not be created
+        """
         event = self.event
         is_limit_reached = event.limit <= event.registered_users_list.all().count() and event.limit is not 0
         self.is_on_wait = is_limit_reached
 
         if event.closed:
-            raise ValidationError('The queue for this event is closed')
+            raise ValidationError(_('is_on_wait: The queue for this event is closed'))
 
         return super(UserEvent, self).save(*args, **kwargs)
 
     def update(self, *args, **kwargs):
+        """
+        Validates model fields upon update.
+        Determines is if the user is allowed to be moved up from the waiting list.
+        :raises ValidationError if the user is moved up from the waiting list and the event limit has been reached.
+                If so, the object will not be updated.
+        """
         event = self.event
         is_limit_reached = event.limit <= event.registered_users_list.all().count() and event.limit is not 0
 
         if not self.is_on_wait and is_limit_reached:
-            raise ValidationError('The queue for this event is full')
-
-        allowed_attributes = {'is_on_wait', 'has_attended'}
-        for name, value in kwargs.items():
-            if name in allowed_attributes:
-                self.name = value
+            raise ValidationError(_('is_on_wait: The queue for this event is full'))
 
         return super(UserEvent, self).save(*args, **kwargs)
 
