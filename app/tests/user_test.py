b5afd32cf4ad096923a0a062492b3c6cb4b8bd89
zuimran <zuimran@stud.ntnu.no>
2019-11-18 18:13:14 +0100

    remove test

--- app/tests/user_test.py
+++ app/tests/user_test.py
@@ -4,11 +4,4 @@ from rest_framework.test import APIRequestFactory
 from rest_framework.test import force_authenticate
 
 class UserTest(APITestCase):
-   def test_user_not_found(self):
-      factory = APIRequestFactory()
-      user = User.objects.get(user_id='zuimran')
-      view = AccountDetail.as_view()
 
-      request = factory.get('/user/zuimran')
-      force_authenticate(request, user=user)
-      response = view(request)
