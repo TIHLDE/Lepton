d1f490498e4ba22362740a062c60f3937e6991cb
zuimran <zuimran@stud.ntnu.no>
2019-11-20 17:39:05 +0100

    all users get a token <3

--- app/content/models/user.py
+++ app/content/models/user.py
@@ -95,8 +95,8 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel, OptionalImage):
     objects = UserManager()
 
 """Genetare token at creation of user"""
-# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-# def create_auth_token(sender, instance=None, created=False, **kwarg):
-#     if created:
-#         Token.objects.create(user=instance)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwarg):
+    if created:
+        Token.objects.create(user=instance)
 
