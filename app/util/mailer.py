52f32e5985803045e58f18106c76d239b286d5f8
Zaim Ul-Abrar Imran <zuimran@stud.ntnu.no>
2020-02-06 15:48:10 +0100

    send mail on user_event change

---
+++ app/util/mailer.py
@@ -0,0 +1,22 @@
+import os
+from django.core.mail import send_mass_mail
+
+
+def send_tihlde_email(subject, message, mail_list):
+  message = (subject, message, os.environ.get('EMAIL_USER'), mail_list)
+  send_mass_mail((message,), fail_silently=False)
+
+
+def send_user_event_mail(is_on_wait, event, user):
+  if is_on_wait:
+    send_tihlde_email(
+      "Du er satt på venteliste for event " + (event),
+      "Inntil videre er du satt på venteliste",
+      user
+    )
+  else:
+    send_tihlde_email(
+      "Du har fått plass på " + (event),
+      "Gratulerer, du har fått plass på eventet " + (event),
+      user
+    )
