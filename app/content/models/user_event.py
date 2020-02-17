e3275e87352011b9a85fa663746b358bb687a95a
Eirik Steira <steiraeirik@gmail.com>
2020-02-17 20:52:12 +0100

    Add functionality for swapping prioritized users with signed up and not prioritized users on the registration list for an event

--- app/content/models/user_event.py
+++ app/content/models/user_event.py
@@ -37,26 +37,44 @@ class UserEvent(BaseModel):
         return super(UserEvent, self).save(*args, **kwargs)
 
     def create(self, *args, **kwargs):
-        """
-        Determines whether user is on the waiting list or not when the instance is created.
+        """ Determines whether user is on the waiting list or not when the instance is created. """
+        self.is_on_wait = self.event_has_waiting_list()
 
-        If the limit is reached or a waiting list exists, the user is automatically put on the waiting list.
-        If the user does not fit in any registration priorities, the user is also put on the waiting list.
-        """
-        event = self.event
-
-        self.is_on_wait = event.limit <= UserEvent.objects.filter(event=event).count() and event.limit is not 0 \
-                          or UserEvent.objects.filter(event=event, is_on_wait=True).exists() \
-                          or not self.is_user_prioritized()
+        if self.is_on_wait and self.event.registration_priorities.exists():
+            self.swap_users()
 
         return super(UserEvent, self).save(*args, **kwargs)
 
-    def is_user_prioritized(self):
+    def event_has_waiting_list(self):
+        """ If the limit is reached or a waiting list exists, the user is automatically put on the waiting list. """
+        event = self.event
+        return event.limit <= UserEvent.objects.filter(event=event).count() and event.limit is not 0 \
+               or UserEvent.objects.filter(event=event, is_on_wait=True).exists()
 
+    def is_user_prioritized(self):
         user_class = UserClass(int(self.user.user_class))
         user_study = UserStudy(int(self.user.user_study))
 
         return self.event.registration_priorities.filter(user_class=user_class, user_study=user_study).exists()
+
+    def swap_users(self):
+        """ Swaps a user with a spot with a prioritized user, if such user exists """
+        event = self.event
+        class_priorities = [priority.user_class.value for priority in event.registration_priorities.all()]
+        study_priorities = [priority.user_study.value for priority in event.registration_priorities.all()]
+
+        try:
+            other_user = UserEvent.objects.filter(
+                event=event,
+                user__user_class__in=class_priorities,
+                user__user_study__in=study_priorities
+            ).first()
+        except UserEvent.DoesNotExist:
+            return
+
+        other_user.is_on_wait = True
+        other_user.save()
+        self.is_on_wait = False
 
     def clean(self):
         """
@@ -82,4 +100,3 @@ class UserEvent(BaseModel):
     def check_registration_has_ended(self):
         if self.event.end_registration_at < today():
             raise ValidationError(_('The registration for this event has ended.'))
-
