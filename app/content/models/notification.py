460864865cffb4ec1ee94777b874df45e6e6d02c
Zaim Imran <zaim.imran@gmail.com>
2020-03-02 19:00:42 +0100

    Update notification.py

--- app/content/models/notification.py
+++ app/content/models/notification.py
@@ -3,7 +3,6 @@ from django.db import models
 from .user import User
 
 class Notification(models.Model):
-    """ Model for notifications """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.CharField(max_length=150)
     read = models.BooleanField(default=False)
