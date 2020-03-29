ce1c06cb4670d8d4369abebe11470556fe053482
zuimran <zuimran@stud.ntnu.no>
2020-03-29 18:26:57 +0200

    further exception handling for cleaner code

--- app/content/views/user_challenge.py
+++ app/content/views/user_challenge.py
@@ -24,14 +24,20 @@ class UserChallengeViewSet(viewsets.ModelViewSet):
 						user = User.objects.get(user_id=request.id)
 						challenge = Challenge.objects.get(
 								id=request.data.get("challenge_id"))
+
 						if UserChallenge.objects.filter(user=user, challenge=challenge).exists():
 								return Response({'detail': 'Challenge already completed.'}, status=400)
+
 						serializer = UserChallengeSerializer(data=request.data)
 						if serializer.is_valid():
 								UserChallenge(user=user, challenge=challenge).save()
 								return Response({'detail': 'Challenge Completed.'}, status=200)
 						else:
 								return Response({'detail': serializer.errors}, status=400)
-
+								
+				except User.DoesNotExist:
+						return Response({'detail': 'Could not find User'}, status=400)
+				except Challenge.DoesNotExist:
+						return Response({'detail': 'Could not find challenge'}, status=400)
 				except Exception as e:
 						return Response({'Error': e}, status=404)
