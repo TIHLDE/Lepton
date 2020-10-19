381d8ac7bfec002b3315a16bf39a8e387ee1f195
Eirik Steira <steiraeirik@gmail.com>
2020-10-19 12:40:53 +0200

    feat(registration): add users email to registration api response  (#180)

--- app/content/serializers/registration.py
+++ app/content/serializers/registration.py
@@ -17,6 +17,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
             "user_id": user.user_id,
             "first_name": user.first_name,
             "last_name": user.last_name,
+            "email": user.email,
             "user_class": user.user_class,
             "user_study": user.user_study,
             "allergy": user.allergy,
