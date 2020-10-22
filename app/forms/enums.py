6a2a75e416f5bf2108f5ff7af45d0bb6648fbe0e
Eirik Steira <eirsteir@stud.ntnu.no>
2020-10-22 19:32:05 +0200

    Add Form, EventForm and field models

--- app/forms/enums.py
+++ app/forms/enums.py
@@ -2,6 +2,12 @@ from enumchoicefield import ChoiceEnum
 
 
 class FormType(ChoiceEnum):
+    GENERAL = "Generelt"
     SURVEY = "Undersøkelse"
     EVALUATION = "Evaluering"
 
+
+class FormFieldType(ChoiceEnum):
+    TEXT_ANSWER = 'Text answer'
+    MULTIPLE_SELECT = 'Multiple select'
+    SINGLE_SELECT = 'Single select'
--- app/forms/urls.py
+++ app/forms/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import include, url
-from django.urls import path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
-# Register content viewpoints here
+
 # router.register("news", NewsViewSet)
-#
-#
-#
-# urlpatterns = [
-#     url(r"", include(router.urls)),
-# ]
+
+
+
+urlpatterns = [
+    # url(r"", include(router.urls)),
+]
