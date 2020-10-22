ce65453fa11f834ac8029d6d532685d320e6a741
Eirik Steira <eirsteir@stud.ntnu.no>
2020-10-22 19:32:05 +0200

    Add Form, EventForm and field models

--- app/forms/models/forms.py
+++ app/forms/models/forms.py
@@ -1,15 +1,40 @@
-from django.db import models
+import uuid
 
+from django.db import models
 from enumchoicefield import EnumChoiceField
 
+from app.content.models.event import Event
 from app.util.models import BaseModel
+from app.forms.enums import FormType, FormFieldType
 
 
 class Form(BaseModel):
 
+    form_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
-    user_study = EnumChoiceField(FormType, default=FormType.DEFAULT)
-
+    type = EnumChoiceField(FormType, default=FormType.GENERAL)
 
     class Meta:
         abstract = True
+
+
+class EventForm(Form):
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="forms")
+    type = EnumChoiceField([FormType.SURVEY, FormType.EVALUATION], default=FormType.SURVEY)
+
+    class Meta:
+        unique_together = (("event", "type"), )
+        verbose_name = "Event form"
+        verbose_name_plural = "Event forms"
+
+
+class Field(models.Model):
+
+    form_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name="fields")
+    title = models.CharField(max_length=200)
+    type = EnumChoiceField(FormFieldType, default=FormType.GENERAL)
+    required = models.BooleanField(default=False)
+
+
