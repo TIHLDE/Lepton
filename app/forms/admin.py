1b1283c6057525a9b5436bc0ab1a54c268ce2de5
Mads Lundegaard <mads.lundegaard@live.no>
2020-10-26 17:56:23 +0100

    Added models to admin

--- app/forms/admin.py
+++ app/forms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from app.forms import models
 # Register your models here.
+admin.site.register(models.Form)
+admin.site.register(models.EventForm)
+admin.site.register(models.Field)
+admin.site.register(models.Option)
+admin.site.register(models.Submission)
+admin.site.register(models.Answer)
