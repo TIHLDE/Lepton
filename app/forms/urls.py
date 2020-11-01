ecab68c87c3f65a6d287a84cd7ca663bebbab771
Mads Lundegaard <mads.lundegaard@live.no>
2020-11-01 17:37:07 +0100

    Started on creating viewset for form

--- app/forms/urls.py
+++ app/forms/urls.py
@@ -1,12 +1,18 @@
+from django.conf.urls import url
 from rest_framework import routers
+from django.urls import include
 
 from app.forms.views.form import FormViewSet
-from app.forms.views.submission import SubmissionViewSet
+#from app.forms.views.submission import SubmissionViewSet
 
 router = routers.DefaultRouter()
 
 
-router.register(r"/", FormViewSet, )
-router.register(
-    r"/(?P<form_id>[0-9a-f-]+)/submissions", SubmissionViewSet
-)
+router.register("", FormViewSet)
+#router.register(
+#    r"/(?P<form_id>[0-9a-f-]+)/submissions", SubmissionViewSet
+#)
+
+urlpatterns = [
+    url(r"", include(router.urls)),
+]
--- app/forms/views/form.py
+++ app/forms/views/form.py
@@ -1,5 +1,12 @@
 from rest_framework import viewsets
 
+from app.content.enums import AdminGroup
+from app.forms.serializers import FormSerializer
+from app.forms.models import Form
+
+from app.forms.permissions import FormPermissions
 
 class FormViewSet(viewsets.ModelViewSet):
-    pass
+    serializer_class = FormSerializer
+    queryset = Form.objects.all()
+    permission_classes = [FormPermissions([AdminGroup.HS, AdminGroup.NOK, AdminGroup.INDEX])]
