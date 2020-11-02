694cceda19f687e71def64735f14c2750c4a8c7d
Mads Lundegaard <mads.lundegaard@live.no>
2020-11-02 19:14:37 +0100

    Here you go mate!

--- app/forms/views/form.py
+++ app/forms/views/form.py
@@ -9,4 +9,4 @@ from app.forms.permissions import FormPermissions
 class FormViewSet(viewsets.ModelViewSet):
     serializer_class = FormSerializer
     queryset = Form.objects.all()
-    permission_classes = [FormPermissions([AdminGroup.HS, AdminGroup.NOK, AdminGroup.INDEX])]
+    #permission_classes = [FormPermissions([AdminGroup.HS, AdminGroup.NOK, AdminGroup.INDEX])]
