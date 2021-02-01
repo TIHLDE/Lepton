8f5b060e207f50c2f50ad3a79d2734160fab5006
Eirik Steira <eirsteir@stud.ntnu.no>
2021-02-01 19:32:20 +0100

    Add integration tests for form retrieval, list and creation

--- app/tests/forms/test_form_integration.py
+++ app/tests/forms/test_form_integration.py
@@ -1,28 +1,15 @@
-import json
 from datetime import timedelta
 
-from django.contrib.auth.models import Group
 from django.utils import timezone
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient
+from rest_framework import status
 
 import pytest
 
 from app.common.enums import AdminGroup
 from app.content.factories.event_factory import EventFactory
-from app.content.factories.user_factory import UserFactory
-from app.forms.models.forms import Form
+from app.util.test_utils import get_api_client
 
-
-def _create_user_client(user_group):
-    test_user = UserFactory(
-        user_id="dev", password="123", first_name="member", last_name="user"
-    )
-    test_user.groups.add(Group.objects.create(name=user_group))
-    token = Token.objects.get(user_id=test_user.user_id)
-    client = APIClient()
-    client.credentials(HTTP_X_CSRF_TOKEN=token)
-    return client
+pytestmark = pytest.mark.django_db
 
 
 def _event_url():
@@ -43,8 +30,16 @@ def _form_detail_url(form):
 
 def _get_form_post_data(form):
     return {
-        # "event": event.pk,
-        "allow_photo": False,
+        "resource_type": "Form",
+        "title": "string",
+        "fields": [
+            {
+                "title": "string",
+                "options": [{"title": "string"}],
+                "type": "SINGLE_SELECT",
+                "required": False,
+            }
+        ],
     }
 
 
@@ -58,10 +53,9 @@ def _get_form_post_data(form):
 
 # GET /events
 # Send med formet (read_only)
-@pytest.mark.django_db
 def test_retrieve_events_does_not_contain_forms():
     event = EventFactory()
-    client = _create_user_client(AdminGroup.INDEX)
+    client = get_api_client(AdminGroup.INDEX)
 
     response = client.get(_event_url())
     event = response.json()
@@ -74,10 +68,9 @@ def test_retrieve_events_does_not_contain_forms():
 # Er brukeren admin?
 # Send med all info fra den over (retrieve) pluss brukerens ubesvarte forms
 # Dette feltet ligger på brukeren som "request.user.unanswered_forms()"
-@pytest.mark.django_db
 def test_retrieve_event_detail_has_form_id_equal_to_the_one_provided():
     event = EventFactory()
-    client = _create_user_client(AdminGroup.INDEX)
+    client = get_api_client(AdminGroup.INDEX)
 
     response = client.get(_event_detail_url(event))
     retrieved_event = response.json()
@@ -88,10 +81,9 @@ def test_retrieve_event_detail_has_form_id_equal_to_the_one_provided():
 
 # PUT /events/<id>/
 # Oppdaterer ikke formet (sendes ikke med fra frontend)
-@pytest.mark.django_db
 def test_update_event_detail_does_not_update_form_ids():
     event = EventFactory()
-    client = _create_user_client(AdminGroup.INDEX)
+    client = get_api_client(AdminGroup.INDEX)
 
     response = client.put(_event_detail_url(event), {"forms": []}, format="json")
     updated_event = response.json()
@@ -101,25 +93,29 @@ def test_update_event_detail_does_not_update_form_ids():
 
 # POST /events/
 # Oppdaterer/lager ikke formet (sendes ikke med fra frontend)
-@pytest.mark.django_db
 def test_create_event():
-    client = _create_user_client(AdminGroup.INDEX)
+    client = get_api_client(AdminGroup.INDEX)
 
-    print(client.post(_event_url(), {
-        "title": "title",
-        "start_date": timezone.now() + timedelta(days=10),
-        "end_date": timezone.now() + timedelta(days=11),
-        "forms": [],
-    }, format="json").json())
+    print(
+        client.post(
+            _event_url(),
+            {
+                "title": "title",
+                "start_date": timezone.now() + timedelta(days=10),
+                "end_date": timezone.now() + timedelta(days=11),
+                "forms": [],
+            },
+            format="json",
+        ).json()
+    )
     raise NotImplementedError()
 
 
-@pytest.mark.django_db
 def test_get_forms():
     raise NotImplementedError()
     form1 = None  # _create_form("foo")
     form2 = None  # _create_form("bar")
-    client = _create_user_client(AdminGroup.INDEX)
+    client = get_api_client(AdminGroup.INDEX)
     response = client.get(_forms_url())
     forms = response.json()
     print(forms)
@@ -144,36 +140,112 @@ def test_get_forms():
     raise NotImplementedError()
 
 
-@pytest.mark.django_db
-def test_get_forms_permissions(user_group):
+def test_list_forms_as_anonymous_user(default_client):
+    """An anonymous user should not be able to list forms."""
+    url = _forms_url()
+    response = default_client.get(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_list_forms_as_member_is_not_permitted(member):
+    """A member should not be able to list forms."""
+    client = get_api_client(user=member)
+    url = _forms_url()
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.parametrize(
+    "group_name",
+    [AdminGroup.HS, AdminGroup.INDEX, AdminGroup.NOK],
+)
+def test_list_forms_as_member_of_nok_hs_or_index(member, group_name):
+    """A user in NOK, HS or Index should be able to list forms."""
+    client = get_api_client(user=member, group_name=group_name)
+    url = _forms_url()
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.parametrize(
+    "group_name",
+    [AdminGroup.SOSIALEN, AdminGroup.PROMO],
+)
+def test_list_forms_as_member_of_sosialen_or_promo(member, group_name):
+    """A user in sosialen or promo should be able to list forms."""
+    client = get_api_client(user=member, group_name=group_name)
+    url = _forms_url()
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_retrieve_form_as_anonymous_user_is_not_permitted(default_client, form):
+    url = _form_detail_url(form)
+    response = default_client.get(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_retrieve_form_as_member(member, form):
+    client = get_api_client(user=member)
+    url = _form_detail_url(form)
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_retrieve_form_as_member_returns_form(member, form):
+    client = get_api_client(user=member)
+    url = _form_detail_url(form)
+    response = client.get(url)
+
+    actual_form = response.json().get("data").get("form")
+
+    assert actual_form.get("id") == form.form_id
+
+
+@pytest.mark.parametrize(
+    "group_name",
+    list(AdminGroup),
+)
+def test_retrieve_form_as_part_of_admin_group(member, group_name):
+    client = get_api_client(user=member, group_name=group_name)
+    url = _forms_url()
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_create_forms_as_anonymous_is_not_permitted():
+    """An anonymous user should not be able to create forms."""
     raise NotImplementedError()
 
 
-@pytest.mark.django_db
-def test_get_form():
-    raise NotImplementedError()
+def test_create_forms_as_member_is_not_permitted(member):
+    """A member should not be able to create forms."""
+    client = get_api_client(user=member)
+    url = _forms_url()
+    response = client.post(url, _get_form_post_data())
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
-@pytest.mark.django_db
-def test_create_forms():
-    raise NotImplementedError()
+@pytest.mark.parametrize(
+    "group_name", [AdminGroup.HS, AdminGroup.INDEX, AdminGroup.NOK]
+)
+def test_create_forms_as_admin_is_permitted(member, group_name):
+    """An admin should be able to create forms."""
+    client = get_api_client(user=member, group_name=group_name)
+    url = _forms_url()
+    response = client.post(url, _get_form_post_data())
+
+    assert response.status_code == status.HTTP_201_CREATED
 
 
-@pytest.mark.django_db
-def test_get_forms_permissions(user_group):
-    raise NotImplementedError()
-
-
-@pytest.mark.django_db
-def test_get_form():
-    raise NotImplementedError()
-
-
-@pytest.mark.django_db
-def test_create_forms():
-    raise NotImplementedError()
-
-
-@pytest.mark.django_db
 def test_update_form():
     raise NotImplementedError()
