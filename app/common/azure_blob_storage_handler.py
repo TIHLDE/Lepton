0101d7528cc6a861e4932c0a8c33efa8db6b1ef1
Max Torre Schau <max.torre.schau@gmail.com>
2021-03-08 18:21:59 +0100

    feat(create container): Create container method

--- app/common/azure_blob_storage_handler.py
+++ app/common/azure_blob_storage_handler.py
@@ -1,3 +1,4 @@
+from azure.core.exceptions import ResourceExistsError
 from azure.storage.blob import BlobServiceClient
 import os
 from dotenv import load_dotenv
@@ -8,13 +9,10 @@ def create_container(name):
     try:
         connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        result = blob_service_client.create_container(name)
-        print(result)
+        blob_service_client.create_container(name)
+    except ResourceExistsError:
+        #TODO:
+        #Better error handling
+        print("Container already exists.")
     except Exception as e:
-        print("Hallo")
-        # TODO:
-        # Better exception handling than this silly little print
-        print(e)
-
-
-create_container("Max")
+        print(f"Error: {e}")
