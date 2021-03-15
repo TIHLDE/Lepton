c9554c19e0ffdc2fc8f6451c9b80a0a506a5b424
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-03-15 18:38:07 +0100

    Tried to check user has attended

--- app/forms/views/submission.py
+++ app/forms/views/submission.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
 from app.common.enums import AdminGroup
 from app.common.permissions import IsMember
+from app.content.models import User
+from app.forms.enums import EventFormType
 from app.forms.serializers.submission import SubmissionSerializer
-from app.forms.models.forms import Submission
+from app.forms.models.forms import Submission, Form, EventForm
+from app.content.views.event import Event
 from app.forms.permissions import FormPermissions
 
 
@@ -17,3 +21,14 @@ class SubmissionViewSet(viewsets.ModelViewSet):
         if self.request.method == ["POST"]:
             self.permission_classes = [IsMember]
         return super().get_permissions()
+
+
+    def create(self, request, *args, **kwargs):
+        form = get_object_or_404(Form.objects.all(), kwargs.get("form_id"))
+        user = get_object_or_404(User.objects.all(), request.user_id)
+
+        if isinstance(form, EventForm):
+            event = get_object_or_404(Event.objects.all(), form.event)
+
+            #if form.type == EventFormType.EVALUATION and user attended.
+
