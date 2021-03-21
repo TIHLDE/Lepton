9d59aea8df085307b56fc6d784bde846a1b738ec
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2021-03-21 15:41:06 +0100

    fix(group): added leader to serializer (#36)

--- app/group/serializers/group.py
+++ app/group/serializers/group.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from app.group.models import Group
+from app.common.enums import MembershipType
+from app.group.models import Group, Membership
 
 
 class DefaultGroupSerializer(serializers.ModelSerializer):
@@ -14,6 +15,8 @@ class DefaultGroupSerializer(serializers.ModelSerializer):
 class GroupSerializer(serializers.ModelSerializer):
     """Serizlier for Groups with lookup by slug field"""
 
+    leader = serializers.SerializerMethodField()
+
     class Meta:
         model = Group
         lookup_field = "slug"
@@ -24,4 +27,19 @@ class GroupSerializer(serializers.ModelSerializer):
             "contact_email",
             "type",
             "permissions",
+            "leader",
         )
+
+    def get_leader(self, obj):
+        try:
+            leader = obj.membership.get(
+                group__slug=obj.slug, membership_type=MembershipType.LEADER
+            )
+            return {
+                "user_id": leader.user.user_id,
+                "first_name": leader.user.first_name,
+                "last_name": leader.user.last_name,
+                "image": leader.user.image,
+            }
+        except Membership.DoesNotExist:
+            return None
