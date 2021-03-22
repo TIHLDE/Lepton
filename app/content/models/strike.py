5f56c66e795692910b2b0fbffdf039c5c426dbac
Zaim Imran <zaim.imran@gmail.com>
2021-03-22 18:46:04 +0100

    refactor: make PR

--- app/content/models/strike.py
+++ app/content/models/strike.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime, timedelta
 
 from django.db import models
 
--- app/content/views/strike.py
+++ app/content/views/strike.py
@@ -1,5 +1,6 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
+from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
