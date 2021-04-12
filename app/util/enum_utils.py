da7c51ed4d4522f353bfb719a15c595b96050f6b
Mads Lundegaard <mads.lundegaard@live.no>
2021-04-12 19:50:39 +0200

    fix(registration): added cleaner logic for handling alumnis on registration

--- app/util/enum_utils.py
+++ app/util/enum_utils.py
@@ -4,6 +4,12 @@ from app.common.enums import UserClass, UserStudy
 class EnumUtils:
     @staticmethod
     def get_user_enums(**kwargs):
-        user_class = UserClass(int(kwargs["user_class"]))
+        print("##########################################")
+        user_class = (
+            UserClass.ALUMNI
+            if int(kwargs["user_class"]) == -1
+            else UserClass(int(kwargs["user_class"]))
+        )
+        print(user_class)
         user_study = UserStudy(int(kwargs["user_study"]))
         return user_class, user_study
