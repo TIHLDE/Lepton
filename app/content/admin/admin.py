962600b217a58a424469e92eee98d4eec2537d18
Mads Lundegaard <mads.lundegaard@live.no>
2021-04-12 18:39:07 +0200

    feat(Admin): Added search fields in registration admin

--- app/content/admin/admin.py
+++ app/content/admin/admin.py
@@ -28,6 +28,7 @@ def admin_delete_registration(modeladmin, request, queryset):
 @admin.register(models.Registration)
 class RegistrationAdmin(admin.ModelAdmin):
     list_display = ("user", "event", "is_on_wait", "has_attended")
+    search_fields = ("user__user_id", "event__title")
     # Enables checks bypassing from the 'Action' dropdown in Registration overview
     actions = [
         admin_delete_registration,
