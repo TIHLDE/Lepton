3855323e2d2266e10c9151f7a48d0afc2c16fd7b
Zenjjim <zaim.imran@gmail.com>
2021-04-25 21:46:35 +0200

    docs: add changelog

--- app/payment/views/refund_form.py
+++ app/payment/views/refund_form.py
@@ -23,29 +23,31 @@ class RefundFormViewSet(viewsets.ModelViewSet):
             req_fields = data_is_valid(data)
             if len(req_fields) > 0:
                 return Response(
-                    f'Required fields {", ".join(req_fields)}',
+                    f'Påkrevde felt {", ".join(req_fields)}',
                     status=status.HTTP_400_BAD_REQUEST,
                 )
             data = format_image_data(data)
             file = create_pdf(data)
             send_refund_mail(data, file)
             return Response(
-                "Receipt is generated and sent to the group's cashier!",
+                "Kvittering er generert og sendt til økonomi ansvarlig!",
                 status=status.HTTP_200_OK,
             )
         except UnsupportedFileException:
             return Response(
-                """One of the file types are not supported.
-                Use PNG, JPEG, GIF, HEIC or PDF""",
+                """En eller flere filer er ikke støttet.
+                Bruk PNG, JPEG, GIF, HEIC or PDF""",
                 status=status.HTTP_400_BAD_REQUEST,
             )
         except RuntimeError as e:
             logging.exception(e)
             return Response(
-                "Could not generate PDF", status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                "Kunne ikke generere PDF",
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
         except Exception as e:
+            logging.exception(e)
             return Response(
-                f"Something unexpected happened. {e}",
+                f"Noe uventet skjedde",
                 status=status.HTTP_400_BAD_REQUEST,
             )
