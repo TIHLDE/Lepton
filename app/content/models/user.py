57fb9f671f0479227f6a6f25b54e2828ccf832c2
Hermann <hermann.owren.elton@hotmail.com>
2021-04-27 17:57:47 +0200

    fix(user-perm): try catch block added to failing attibute check

--- app/content/models/user.py
+++ app/content/models/user.py
@@ -111,9 +111,13 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel, OptionalImage):
 
     @classmethod
     def has_list_permission(cls, request):
-        return check_has_access(cls.has_access, request) or len(
-            request.user.membership.filter(membership_type=MembershipType.LEADER)
-        )
+        try:
+                
+            return check_has_access(cls.has_access, request) or len(
+                request.user.membership.filter(membership_type=MembershipType.LEADER)
+            )
+        except AttributeError:
+            return  check_has_access(cls.has_access, request)
 
     @staticmethod
     def has_read_permission(request):
