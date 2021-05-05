a5a059dbd9def4f0a7e235aa513b83c59d1a266e
Hermann <hermann.owren.elton@hotmail.com>
2021-05-05 13:04:20 +0200

    format

--- app/group/admin.py
+++ app/group/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-#from django.contrib.auth.models import Group as auth_group
 
 from app.group import models
+
+# from django.contrib.auth.models import Group as auth_group
+
 
 admin.site.register(models.Group)
 admin.site.register(models.Membership)
 admin.site.register(models.MembershipHistory)
-#admin.site.unregister(auth_group)
+# admin.site.unregister(auth_group)
