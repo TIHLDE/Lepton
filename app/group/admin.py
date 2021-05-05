bd88032bdbbb631a0dcef8651254c31358979b4d
Hermann <hermann.owren.elton@hotmail.com>
2021-05-05 12:59:59 +0200

    readded auth group

--- app/group/admin.py
+++ app/group/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group as auth_group
+#from django.contrib.auth.models import Group as auth_group
 
 from app.group import models
 
 admin.site.register(models.Group)
 admin.site.register(models.Membership)
 admin.site.register(models.MembershipHistory)
-admin.site.unregister(auth_group)
+#admin.site.unregister(auth_group)
