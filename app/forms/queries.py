c99cb968022f059b8ba43229596fc94fdd6f0eaa
Eirik Steira <eirsteir@stud.ntnu.no>
2021-09-23 14:40:22 +0200

    Implemen graphql dry permissions

--- app/forms/queries.py
+++ app/forms/queries.py
@@ -3,6 +3,7 @@ from rest_framework.generics import get_object_or_404
 
 import graphene
 
+from app.common.generics import DRYGraphQLPermissions
 from app.forms.models import Form
 from app.forms.types import FormUnionType
 
@@ -10,22 +11,24 @@ from app.forms.types import FormUnionType
 class RetrieveModelMixin:
     @classmethod
     def resolve_retrieve(cls, root, info, **kwargs):
-        return cls.get_object(info, **kwargs)
+        return cls.get_object(request=info.context, **kwargs)
 
 
 class ListModelMixin:
     @classmethod
     def resolve_list(cls, root, info, **kwargs):
-        cls.check_permissions(info)
+        cls.check_permissions(request=info.context)
         return cls.get_queryset()
 
 
+# TODO: implement basic library checks
 class GenericQuery:
     queryset = None
+    permission_classes = []
     lookup_field = "pk"
 
     @classmethod
-    def get_object(cls, info, **kwargs):
+    def get_object(cls, request, **kwargs):
 
         # Perform the lookup filtering.
         lookup_url_kwarg = cls.lookup_field
@@ -41,39 +44,54 @@ class GenericQuery:
         obj = get_object_or_404(cls.queryset, **filter_kwargs)
 
         # May raise a permission denied
-        cls.check_object_permissions(info, obj)
+        cls.check_object_permissions(request, obj)
 
         return obj
-
-    # TODO: Implement from dry_rest_permissions.generics.DRYPermissions and use it here
-    @classmethod
-    def check_permissions(cls, info):
-        model_class = cls.queryset.model
-        has_permission = model_class.has_list_permission(request=info.context)
-
-        if not has_permission:
-            cls.permission_denied(info)
 
     @classmethod
     def get_queryset(cls):
         return cls.queryset
 
-    # TODO: Implement from dry_rest_permissions.generics.DRYPermissions and use it here
     @classmethod
-    def check_object_permissions(cls, info, obj):
-        has_retrieve_permission = obj.has_retrieve_permission(request=info.context)
-
-        if not has_retrieve_permission:
-            cls.permission_denied(info)
-
-    @classmethod
-    def permission_denied(cls, info, message=None, code=None):
+    def permission_denied(cls, context, message=None, code=None):
         """
         If request is not permitted, determine what kind of exception to raise.
         """
-        if info.context.user and not info.context.user.is_authenticated:
+        if context.user and not context.user.is_authenticated:
             raise NotAuthenticated()
         raise PermissionDenied(detail=message, code=code)
+
+    @classmethod
+    def check_permissions(cls, request):
+        """
+        Check if the request should be permitted.
+        Raises an appropriate exception if the request is not permitted.
+        """
+        for permission in cls.get_permissions():
+            if not permission.has_permission(request, cls):
+                cls.permission_denied(
+                    request,
+                    message=getattr(permission, "message", None),
+                    code=getattr(permission, "code", None),
+                )
+
+    @classmethod
+    def check_object_permissions(cls, request, obj):
+        """
+        Check if the request should be permitted for a given object.
+        Raises an appropriate exception if the request is not permitted.
+        """
+        for permission in cls.get_permissions():
+            if not permission.has_object_permission(request, cls, obj):
+                cls.permission_denied(
+                    request,
+                    message=getattr(permission, "message", None),
+                    code=getattr(permission, "code", None),
+                )
+
+    @classmethod
+    def get_permissions(cls):
+        return cls.permission_classes
 
 
 class ModelQuery(RetrieveModelMixin, ListModelMixin, GenericQuery):
@@ -82,6 +100,7 @@ class ModelQuery(RetrieveModelMixin, ListModelMixin, GenericQuery):
 
 class FormQuery(ModelQuery):
     queryset = Form.objects.all()
+    permission_classes = [DRYGraphQLPermissions]
     lookup_field = "id"
 
     forms = graphene.List(FormUnionType)
