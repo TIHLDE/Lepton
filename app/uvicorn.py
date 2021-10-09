c2b28631cf21f9272a0b303ae1c5c6e2c993c9fd
olros <olafrosendahl@gmail.com>
2021-10-09 15:06:20 +0200

    format

--- app/uvicorn.py
+++ app/uvicorn.py
@@ -1,5 +1,7 @@
 from uvicorn.workers import UvicornWorker as DefaultUvicornWorker
 
+
 class UvicornWorker(DefaultUvicornWorker):
     """ Custom UvicornWorker-class used to turn of lifespan as it isn't supported by Django """
+
     CONFIG_KWARGS = {"lifespan": "off"}
