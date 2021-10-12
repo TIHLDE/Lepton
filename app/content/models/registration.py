599320f9b0261322f0c02def61923a196ff407db
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-10-12 18:50:44 +0200

    Added check for strike enforcement

--- app/content/models/registration.py
+++ app/content/models/registration.py
@@ -74,9 +74,7 @@ class Registration(BaseModel, BasePermissionModel):
         return check_has_access(self.has_access, request,)
 
     def has_object_retrieve_permission(self, request):
-        if self.user.user_id == request.id:
-            return True
-        return check_has_access(self.has_access, request,)
+        return self.has_object_destroy_permission(request)
 
     def __str__(self):
         return (
@@ -123,8 +121,11 @@ class Registration(BaseModel, BasePermissionModel):
         return super(Registration, self).save(*args, **kwargs)
 
     def create(self):
-        self._abort_for_unanswered_evaluations()
-        self.strike_handler()
+        # Only cancel registration if the event enforces previous strikes.
+        if self.event.enforces_previous_strikes:
+            self._abort_for_unanswered_evaluations()
+            self.strike_handler()
+
         self.clean()
         self.is_on_wait = self.event.is_full
 
