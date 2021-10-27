9ff5d27cab6b8775d96666e9cce5fe996f15a11c
CJGutz <cjgutzkow@gmail.com>
2021-10-27 20:03:34 +0200

    added 201 status response

--- app/gallery/views/picture.py
+++ app/gallery/views/picture.py
@@ -26,7 +26,7 @@ class PictureViewSet(viewsets.ModelViewSet):
         )
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(
                 {"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
