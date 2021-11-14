e1ce64fb4249623e7d16201dded7a769fcbdbbcb
CJGutz <cjgutzkow@gmail.com>
2021-11-14 22:48:38 +0100

    added test for page order

--- app/tests/content/test_page_integration.py
+++ app/tests/content/test_page_integration.py
@@ -190,3 +190,31 @@ def test_create_as_group_user_fails_when_no_root_exists(admin_user):
     response = client.post(url, data=data)
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_pages_do_not_reorder_with_initial_correct_order(parent_page):
+    """Test that pages don't switch position when order starts correct"""
+
+    page_created_first = PageFactory(parent=parent_page, order=0)
+    page_created_second = PageFactory(parent=parent_page, order=1)
+
+    first_child = parent_page.children.all()[0]
+    second_child = parent_page.children.all()[1]
+
+    assert page_created_first == first_child
+    assert page_created_second == second_child
+
+
+@pytest.mark.django_db
+def test_pages_reorder_with_initial_wrong_order(parent_page):
+    """Test that updating pages work, by creating pages in wrong order"""
+
+    page_created_first = PageFactory(parent=parent_page, order=1)
+    page_created_second = PageFactory(parent=parent_page, order=0)
+
+    first_child = parent_page.children.all()[0]
+    second_child = parent_page.children.all()[1]
+
+    assert not page_created_first == first_child
+    assert not page_created_second == second_child
