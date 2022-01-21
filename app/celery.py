f6b429e5145ff653a70dafc47906d77823d52ea3
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-01-21 12:09:30 +0100

    feat(celery): setup cronjobs and logging

--- app/celery.py
+++ app/celery.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from django.conf import settings
+
+from app.util.tasks import BaseTask
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
@@ -18,15 +21,25 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+schedule = {
+    'run-every-60-seconds': {
+        'task': 'app.celery.debug_task',
+        'schedule': 60.0
+    },
+}
+
 app.conf.update(
+    beat_schedule=schedule,
     task_serializer="json",
     accept_content=["json"],  # Ignore other content
     result_serializer="json",
-    timezone="Europe/Oslo",
-    enable_utc=True,
+    timezone=settings.TIME_ZONE,
+    enable_utc=True
 )
 
 
-@app.task(bind=True)
-def debug_task(self):
-    print("Request: {0!r}".format(self.request))
+@app.task(bind=True, base=BaseTask)
+def debug_task(self, *args, **kwargs):
+    from app.util.utils import now
+
+    self.logger.info(f"Test {now()}")
---
+++ app/util/tasks.py
@@ -0,0 +1,11 @@
+import celery
+
+class BaseTask(celery.Task):
+    """
+    This base task supplies the logger to the underlying worker on init.
+    The logger may then be used like this: `self.logger.info("Hi")`
+    """
+
+    def __init__(self):
+        import logging
+        self.logger = logging.getLogger(__name__)
