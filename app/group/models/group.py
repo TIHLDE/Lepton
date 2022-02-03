06e34fcef9abd166c860de90aea088e6d542835e
Omer Jonuzi <omerjonuzi7@gmail.com>
2022-02-03 17:16:47 +0100

    refactor(user) removing unintentional changes from other branch

--- app/group/models/group.py
+++ app/group/models/group.py
@@ -21,14 +21,6 @@ class Group(OptionalImage, BaseModel, BasePermissionModel):
     fine_info = models.TextField(default="", blank=True)
     type = EnumChoiceField(GroupType, default=GroupType.OTHER)
     fines_activated = models.BooleanField(default=False)
-    members = models.ManyToManyField(
-        User,
-        through="Membership",
-        through_fields=("group", "user"),
-        blank=True,
-        default=None,
-        verbose_name="group members",
-    )
     fines_admin = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
