df5b737e39789f7db9bad3a09fb136237bf129cd
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-02-28 11:45:05 +0100

    fix(events): queryset filtering

--- app/content/filters/event.py
+++ app/content/filters/event.py
@@ -5,13 +5,12 @@ from django_filters.rest_framework import (
 )
 
 from app.content.models import Event
-from app.util.utils import midday, now, yesterday
+from app.util.utils import now
 
 
 class EventFilter(FilterSet):
     """ Filters events by category and expired. Works with search query """
 
-    expired = BooleanFilter(method="filter_expired", label="Newest")
     end_range = DateTimeFilter(field_name="start_date", lookup_expr="lte")
     start_range = DateTimeFilter(field_name="end_date", lookup_expr="gte")
     open_for_sign_up = BooleanFilter(
@@ -21,15 +20,7 @@ class EventFilter(FilterSet):
 
     class Meta:
         model = Event
-        fields = ["category", "expired", "organizer", "end_range", "start_range"]
-
-    def filter_expired(self, queryset, name, value):
-        midday_yesterday = midday(yesterday())
-        midday_today = midday(now())
-        time = midday_today if midday_today < now() else midday_yesterday
-        if value:
-            return queryset.filter(end_date__lt=time).order_by("-start_date")
-        return queryset.filter(end_date__gte=time)
+        fields = ["category", "organizer", "end_range", "start_range"]
 
     def filter_open_for_sign_up(self, queryset, name, value):
         if value:
