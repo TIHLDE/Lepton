0cfe7c3fa9da0e624cee51310fe6cad28156fb16
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-03-13 12:42:56 +0100

    refactor(flower): remove as it's no longer useful (#508)

--- app/urls.py
+++ app/urls.py
@@ -21,8 +21,6 @@ from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
-from app.flower import flower_admin_site
-
 schema_view = get_schema_view(
     openapi.Info(
         title="LEPTON API",
@@ -36,7 +34,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("flower/", flower_admin_site.urls),
     path(
         "",
         schema_view.with_ui("swagger", cache_timeout=0),
