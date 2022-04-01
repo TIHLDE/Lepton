6f7ccf606f7fd5b0826d28764ff3c918a135da82
Carl Johan Gützkow <70779496+CJGutz@users.noreply.github.com>
2022-04-01 18:40:25 +0200

    fix(badges): completion percentage does not count unregistered user (#522)
    * fix(badges): completion percentage does not count unregistered user
    
    * fix(badges_test): removed unused imports

--- app/tests/badge/test_badge_and_category_integration.py
+++ app/tests/badge/test_badge_and_category_integration.py
@@ -5,6 +5,10 @@ from rest_framework import status
 import pytest
 
 from app.badge.factories import BadgeFactory
+from app.badge.factories.user_badge_factory import UserBadgeFactory
+from app.common.enums import Groups
+from app.content.factories.user_factory import UserFactory
+from app.util.test_utils import add_user_to_group_with_name
 from app.util.utils import now
 
 API_BADGE_BASE_URL = "/badges/"
@@ -78,3 +82,29 @@ def test_no_badges_are_shown_when_none_are_public(api_client, admin_user):
     print(results)
 
     assert len(results) == 1
+
+
+@pytest.mark.django_db
+def test_completion_percentage_excludes_all_unregistered_users(badge, api_client):
+    """Unregistered users are all users who do not have a membership in the TIHLDE group
+    When calculating completing percentage, only members in this group are counted.
+    Creates 4 valid users plus one unregistered user. Result should be 25 == 1/4 * 100"""
+
+    member_with_badge = UserFactory()
+
+    add_user_to_group_with_name(UserFactory(), Groups.TIHLDE)
+    add_user_to_group_with_name(UserFactory(), Groups.TIHLDE)
+    add_user_to_group_with_name(UserFactory(), Groups.TIHLDE)
+    add_user_to_group_with_name(member_with_badge, Groups.TIHLDE)
+
+    UserBadgeFactory(badge=badge, user=member_with_badge)
+    member_client = api_client(member_with_badge)
+
+    UserFactory()
+
+    url = _get_badges_url()
+
+    response = member_client.get(url)
+    response = response.json()
+
+    assert response.get("results")[0].get("total_completion_percentage") == 25.0
