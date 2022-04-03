2327da102cb02b7279f1c4107e4fcac4b4a96bff
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-04-03 13:53:06 +0200

    fix: swagger

--- app/urls.py
+++ app/urls.py
@@ -16,29 +16,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import permissions
-
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="LEPTON API",
-        default_version="v1",
-        description="API documentation for tihlde.org",
-        contact=openapi.Contact(email="index@tihlde.org"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
-    path(
-        "",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
     path("admin/", admin.site.urls),
     path("", include("rest_framework.urls")),
     # Our endpoints
