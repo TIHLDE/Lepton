353cf21d9ed22d52ee53d87565a35d9ea2dc385b
norgor <7034394+norgor@users.noreply.github.com>
2022-04-21 15:31:21 +0000

    is ready? :shipit: :feelsgood:

--- app/emoji/admin.py
+++ app/emoji/admin.py
@@ -6,14 +6,12 @@ from app.emoji.models.custom_short_name import CustomShortName
 
 class InlineCustomShortNameAdmin(admin.TabularInline):
     model = CustomShortName
-    pass
 
 
 @admin.register(models.CustomEmoji)
 class CustomEmojiAdmin(admin.ModelAdmin):
     fields = ("img",)
     inlines = [InlineCustomShortNameAdmin]
-    pass
 
 
 @admin.register(models.CustomShortName)
