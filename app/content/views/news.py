d0da7ab5c8934e7733decff358ec6f320de80978
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2022-05-12 18:27:17 +0200

    refactor: change news list endpoint to return less info (#583)
    refactor: change list endpoint to return less info

--- app/content/views/news.py
+++ app/content/views/news.py
@@ -5,7 +5,7 @@ from app.common.pagination import BasePagination
 from app.common.permissions import BasicViewPermission
 from app.common.viewsets import BaseViewSet
 from app.content.models import News
-from app.content.serializers import NewsSerializer
+from app.content.serializers import NewsSerializer, SimpleNewsSerializer
 
 
 class NewsViewSet(BaseViewSet):
@@ -14,6 +14,11 @@ class NewsViewSet(BaseViewSet):
     serializer_class = NewsSerializer
     permission_classes = [BasicViewPermission]
     pagination_class = BasePagination
+
+    def get_serializer_class(self):
+        if hasattr(self, "action") and self.action == "list":
+            return SimpleNewsSerializer
+        return super().get_serializer_class()
 
     def destroy(self, request, *args, **kwargs):
         super().destroy(request, *args, **kwargs)
