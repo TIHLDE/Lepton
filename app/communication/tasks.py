a1a6b8944659c8896f5925bc2c8f6e4c260afd2e
norgor <7034394+norgor@users.noreply.github.com>
2022-09-29 16:27:14 +0000

    Do not use in mail send

--- app/communication/tasks.py
+++ app/communication/tasks.py
@@ -10,13 +10,13 @@ def send_due_mails(self, *args, **kwargs):
     from app.util.utils import now
 
     mails = Mail.objects.select_for_update().filter(eta__lt=now())
-    total_mails = None
+    total_mails = 0
     mails_sent = 0
     with transaction.atomic():
-        total_mails = mails.count()
         for mail in mails:
             is_success = mail.send()
             if is_success:
                 mails_sent += 1
+            total_mails += 1
 
     self.logger.info(f"Successfully sent: {mails_sent}/{total_mails.count()} mails")
