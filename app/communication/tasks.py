6829518fcf57c2fd4e830d972f203429e0f54309
norgor <7034394+norgor@users.noreply.github.com>
2022-09-29 16:09:45 +0000

    Lock mail rows that are being sent.

--- app/communication/tasks.py
+++ app/communication/tasks.py
@@ -1,5 +1,6 @@
 from app.celery import app
 from app.util.tasks import BaseTask
+from django.db import transaction
 
 
 @app.task(bind=True, base=BaseTask)
@@ -7,14 +8,14 @@ def send_due_mails(self, *args, **kwargs):
     from app.communication.models.mail import Mail
     from app.util.utils import now
 
-    mails_to_send = Mail.objects.filter(eta__lt=now())
-    mails_successfully_sent = 0
+    mails = Mail.objects.select_for_update().filter(eta__lt=now())
+    total_mails = None
+    mails_sent = 0
+    with transaction.atomic():
+        total_mails = mails.count()
+        for mail in mails:
+            is_success = mail.send()
+            if is_success:
+                mails_sent += 1
 
-    for mail in mails_to_send:
-        is_success = mail.send()
-        if is_success:
-            mails_successfully_sent += 1
-
-    self.logger.info(
-        f"Successfully sent: {mails_successfully_sent}/{mails_to_send.count()} mails"
-    )
+    self.logger.info(f"Successfully sent: {mails_sent}/{total_mails.count()} mails")
