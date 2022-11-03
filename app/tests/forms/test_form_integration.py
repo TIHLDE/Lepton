03c5bede55bbab9ec0ef5b4e486c24e035aa0068
Jarand Romestrand <jarandjr@stud.ntnu.no>
2022-11-03 17:47:43 +0100

    added some spaces where there should be

--- app/tests/forms/test_form_integration.py
+++ app/tests/forms/test_form_integration.py
@@ -5,7 +5,7 @@ import pytest
 from app.common.enums import AdminGroup
 from app.forms.models.forms import Field
 from app.forms.tests.form_factories import FieldFactory, FormFactory
-from app.util.test_utils import get_api_client, add_user_to_group_with_name
+from app.util.test_utils import add_user_to_group_with_name, get_api_client
 
 pytestmark = pytest.mark.django_db
 
@@ -129,6 +129,7 @@ def test_list_forms_as_member_is_not_permitted(member):
 
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
+
 def test_list_forms_as_nok_member_is_permitte(member):
     """A nok member should be able to list forms."""
     add_user_to_group_with_name(member, AdminGroup.NOK)
@@ -137,6 +138,7 @@ def test_list_forms_as_nok_member_is_permitte(member):
     response = client.get(url)
 
     assert response.status_code == status.HTTP_200_OK
+
 
 @pytest.mark.parametrize(
     ("group_name", "expected_status_code"),
@@ -220,6 +222,7 @@ def test_create_forms_as_member_is_not_permitted(form, member):
 
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
+
 def test_create_forms_as_nok_member_is_permitted(form, member):
     """A nok member should be able to create forms."""
     add_user_to_group_with_name(member, AdminGroup.NOK)
@@ -228,6 +231,7 @@ def test_create_forms_as_nok_member_is_permitted(form, member):
     response = client.post(url, _get_form_post_data(form))
 
     assert response.status_code == status.HTTP_201_CREATED
+
 
 @pytest.mark.parametrize("group_name", [AdminGroup.HS, AdminGroup.INDEX])
 def test_create_forms_as_admin_is_permitted(form, member, group_name):
@@ -255,6 +259,7 @@ def test_update_form_as_member_is_not_permitted(member, form):
 
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
+
 def test_update_form_as_nok_member_is_permitted(member, form):
     """A nok member should be allowed to update forms."""
     add_user_to_group_with_name(member, AdminGroup.NOK)
@@ -262,7 +267,7 @@ def test_update_form_as_nok_member_is_permitted(member, form):
     url = _get_form_detail_url(form)
     response = client.put(url, _get_form_update_data(form))
 
-    assert response.status_code == status.HTTP_200_OK    
+    assert response.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.parametrize(
@@ -482,6 +487,7 @@ def test_delete_form_as_member_is_not_permitted(member, form):
     response = client.delete(url)
 
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
 
 def test_delete_form_as_nok_member_is_permitted(member, form):
     """Nok members should be allowed to delete forms."""
