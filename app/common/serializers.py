e3591bd5d66dae6a9b8f7d44b1609c33fcad9e61
Thomas <thomassvendal@gmail.com>
2022-11-17 18:12:47 +0100

    fix done

--- app/common/serializers.py
+++ app/common/serializers.py
@@ -5,6 +5,6 @@ from app.common.file_handler import replace_file
 
 class BaseModelSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
-        if hasattr(instance, "image"):
+        if hasattr(instance, "image") and instance.image != validated_data.get("image"):
             replace_file(instance.image, validated_data.get("image", None))
         return super().update(instance, validated_data)
