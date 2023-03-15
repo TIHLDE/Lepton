5bcd237ab055522131a6b219133d4dd8d1fa00cf
Mads Nylund <madsnyl@gmail.com>
2023-03-15 16:22:46 +0100

    started on test for forcing vipps payment

--- app/payment/views/vipps_callback.py
+++ app/payment/views/vipps_callback.py
@@ -25,3 +25,22 @@ def vipps_callback(request):
         return status
     except Exception as e:
         print(e)
+
+def force_payment(order_id):
+    try:
+        access_token = get_new_access_token()[1]
+        url = f"https://apitest.vipps.no/ecomm/v2/integration-test/payments/{order_id}/approve"
+        headers = {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": settings.VIPPS_SUBSCRIPTION_KEY,
+            "Authorization": "Bearer " + access_token,
+            "Merchant-Serial-Number": settings.VIPPS_MERCHANT_SERIAL_NUMBER,
+            "Cookie": "fpc=AqiUsXVZL3NFr4JO1-F_-NRQ2zIJAQAAAGhUfNsOAAAA; stsservicecookie=estsfd; x-ms-gateway-slice=estsfd",
+        }
+
+        res = requests.post(url, headers=headers)
+        status_code = res.status_code
+        json = res.json()
+        return (json, status_code)
+    except Exception as e:
+        print(e)
--- app/tests/payment/test_vipps_callback.py
+++ app/tests/payment/test_vipps_callback.py
@@ -2,7 +2,7 @@ import pytest
 from app.payment.factories.order_factory import OrderFactory
 from app.payment.factories.paid_event_factory import PaidEventFactory
 from app.payment.models.order import Order
-from app.payment.views.vipps_callback import vipps_callback
+from app.payment.views.vipps_callback import vipps_callback, force_payment
 from app.util.test_utils import get_api_client
 from rest_framework import status
 
@@ -35,5 +35,23 @@ def test_if_order_gets_updated_by_vipps_callback(member, paid_event):
     new_status = vipps_callback({"orderId": order_id})
 
     assert order_status == new_status
+
+@pytest.mark.django_db
+def test_force_vipps_payment(member, paid_event):
+    """A member should be able to create a registration for themselves."""
+    data = _get_registration_post_data(member, paid_event)
+    client = get_api_client(user=member)
+
+    url = _get_registration_url(event=paid_event.event)
+    response = client.post(url, data=data)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    order = Order.objects.all()[0]
+    json, status_code = force_payment(order.order_id)
+    print(status_code)
+    print(json)
+
+
 
     
