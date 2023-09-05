775754b93b50b90673b15a270ff7a50450f1ca11
Martin Clementz <martcl@stud.ntnu.no>
2023-09-05 17:42:23 +0200

    feat: banner search

--- app/communication/views/banner.py
+++ app/communication/views/banner.py
@@ -1,3 +1,6 @@
+from django_filters import BooleanFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework.filterset import FilterSet
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -10,11 +13,33 @@ from app.communication.serializers.banner import BannerSerializer
 from app.util.utils import now
 
 
+class BannerFilter(FilterSet):
+    is_visible = BooleanFilter(method="filter_isVisible")
+    is_expired = BooleanFilter(method="filter_isExpired")
+
+    class Meta:
+        model = Banner
+        fields = ["is_visible", "is_expired"]
+
+    def filter_isVisible(self, queryset, name, value):
+        if value:
+            return queryset.filter(visible_from__lte=now(), visible_until__gte=now())
+        return queryset
+
+    def filter_isExpired(self, queryset, name, value):
+        if value:
+            return queryset.filter(visible_until__lt=now())
+        return queryset
+
+
 class BannerViewSet(BaseViewSet):
     serializer_class = BannerSerializer
     pagination_class = BasePagination
     queryset = Banner.objects.all()
     permission_classes = [BasicViewPermission]
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BannerFilter
 
     @action(
         detail=False,
