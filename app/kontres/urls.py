98be36b6326e831e162de6bbb3b81675dd9aa8b4
eriskjel <eriskjel@stud.ntnu.no>
2023-10-02 23:38:04 +0200

    added create_reservation to url path

--- app/kontres/urls.py
+++ app/kontres/urls.py
@@ -1,7 +1,6 @@
-from rest_framework import routers
+from app.kontres.views.create_reservation import create_reservation
+from django.urls import path
 
-
-router = routers.DefaultRouter()
-
-
-urlpatterns = []
+urlpatterns = [
+    path('v1/create_reservation/', create_reservation, name='create_reservation'),
+]
