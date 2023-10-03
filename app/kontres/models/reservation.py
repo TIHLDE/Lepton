bb21202718e0afce03312c4776bdfa8719f62884
eriskjel <eriskjel@stud.ntnu.no>
2023-10-03 21:26:58 +0200

    made "Kontoret" default value of a reservation for now and added migration

--- app/kontres/models/reservation.py
+++ app/kontres/models/reservation.py
@@ -1,10 +1,12 @@
 from django.db import models
 from app.content.models import User
 from app.kontres.reservation_state import ReservationStateEnum
+from app.kontres.models.bookable_item import BookableItem
 
 
 class Reservation(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations')
+    bookable_item = models.ForeignKey(BookableItem, on_delete=models.CASCADE, related_name='reservations', default=1)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     state = models.CharField(max_length=15, choices=ReservationStateEnum.choices, default=ReservationStateEnum.PENDING)
@@ -12,6 +14,6 @@ class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.state} - Reservation request by {self.author}"
+        return f"{self.state} - Reservation request by {self.author} to book {self.bookable_item.name}"
 
 
