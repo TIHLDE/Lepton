7233ae2936d08396906ae2979f68c80ee56c626e
eriskjel <eriskjel@stud.ntnu.no>
2023-10-03 22:29:21 +0200

    updated urls.py with the newest endpoints

--- app/kontres/urls.py
+++ app/kontres/urls.py
@@ -1,6 +1,11 @@
 from app.kontres.views.create_reservation import create_reservation
+from app.kontres.views.fetch_reservations import fetch_all_reservations
+from app.kontres.views.edit_reservation import edit_reservation
 from django.urls import path
 
 urlpatterns = [
-    path('v1/create_reservation/', create_reservation, name='create_reservation'),
+    path('create_reservation/', create_reservation, name='create_reservation'),
+    path('fetch_all_reservations/', fetch_all_reservations, name='fetch_all_reservations'),
+    path('edit_reservation/<int:reservation_id>/', edit_reservation, name='edit_reservation')
+
 ]
