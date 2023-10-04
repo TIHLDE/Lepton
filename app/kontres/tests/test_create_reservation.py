02aa0b13de0c593d99a4647455ff212666381f61
Erik Skjellevik <eriskjel@stud.ntnu.no>
2023-10-04 23:20:19 +0200

    fixed and added more tests in test_create_reservation.py

--- app/kontres/tests/test_create_reservation.py
+++ app/kontres/tests/test_create_reservation.py
@@ -16,8 +16,9 @@ def test_bookable_item():
 
 @pytest.fixture()
 def create_valid_reservation(client, test_user, test_bookable_item):
-    response = client.post(f'/kontres/create_reservation/?bookable_item={test_bookable_item.id}', {
+    response = client.post(f'/kontres/create_reservation/', {
         'author': test_user.user_id,
+        'bookable_item_id': test_bookable_item.id,
         'start_time': '2023-10-10T10:00:00Z',
         'end_time': '2023-10-10T11:00:00Z',
     }, format='json')
@@ -29,21 +30,39 @@ def test_user_can_create_reservation(test_user, test_bookable_item):
     client = APIClient()
     client.force_authenticate(user=test_user)
 
-    response = client.post(f'/kontres/create_reservation/?bookable_item={test_bookable_item.id}', {
+    response = client.post(f'/kontres/create_reservation/', {
         'author': test_user.user_id,
+        'bookable_item': test_bookable_item.id,
         'start_time': '2023-10-10T10:00:00Z',
         'end_time': '2023-10-10T11:00:00Z',
     }, format='json')
 
     assert response.status_code == 201
     assert response.data['author'] == test_user.user_id
-    assert response.data['bookable_item']['id'] == test_bookable_item.id
+    assert response.data['bookable_item'] == test_bookable_item.id
+    assert response.data['state'] == 'PENDING'
+
+
+@pytest.mark.django_db
+def test_user_cannot_create_confirmed_reservation(test_user, test_bookable_item):
+    client = APIClient()
+    client.force_authenticate(user=test_user)
+
+    response = client.post(f'/kontres/create_reservation/', {
+        'author': test_user.user_id,
+        'bookable_item': test_bookable_item.id,
+        'start_time': '2023-10-10T10:00:00Z',
+        'end_time': '2023-10-10T11:00:00Z',
+        'state': 'CONFIRMED'
+    }, format='json')
+
     assert response.data['state'] == 'PENDING'
 
 
 @pytest.mark.django_db
 def test_user_cannot_create_reservation_without_author(client, test_bookable_item):
-    response = client.post(f'/kontres/create_reservation/?bookable_item={test_bookable_item.id}', {
+    response = client.post(f'/kontres/create_reservation/', {
+        'bookable_item_id': test_bookable_item.id,
         'start_time': '2023-10-10T10:00:00Z',
         'end_time': '2023-10-10T11:00:00Z',
     }, format='json')
@@ -53,8 +72,9 @@ def test_user_cannot_create_reservation_without_author(client, test_bookable_ite
 
 @pytest.mark.django_db
 def test_user_cannot_create_reservation_with_invalid_date_format(client, test_user, test_bookable_item):
-    response = client.post(f'/kontres/create_reservation/?bookable_item={test_bookable_item.id}', {
+    response = client.post(f'/kontres/create_reservation/', {
         'author': test_user.user_id,
+        'bookable_item_id': test_bookable_item.id,
         'start_time': 'invalid_date_format',
         'end_time': '2023-10-10T11:00:00Z',
     }, format='json')
