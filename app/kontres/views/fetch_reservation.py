c8fce2073003e0c1397f431bbdf1268cf2149cfd
eriskjel <eriskjel@stud.ntnu.no>
2023-10-04 13:33:51 +0200

    created endpoint to fetch reseervation by id

---
+++ app/kontres/views/fetch_reservation.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from app.kontres.serializer.reservation_seralizer import ReservationSerializer
+from app.kontres.models.reservation import Reservation
+from django.core.exceptions import ObjectDoesNotExist
+
+
+@api_view(['GET'])
+def fetch_reservation(request):
+    try:
+        reservation_id = request.GET.get('reservation_id')
+        if not reservation_id:
+            return Response({"error": "Reservation ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        reservation = Reservation.objects.get(id=reservation_id)
+        serializer = ReservationSerializer(reservation, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    except ObjectDoesNotExist:
+        return Response({"error": "Reservation not found"}, status=status.HTTP_404_NOT_FOUND)
