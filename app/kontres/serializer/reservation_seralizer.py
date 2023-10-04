18d4e5fd1259f599131106929f7f0f2452e3064c
eriskjel <eriskjel@stud.ntnu.no>
2023-10-04 13:32:07 +0200

    added bookable item object to reservation serializer

--- app/kontres/serializer/reservation_seralizer.py
+++ app/kontres/serializer/reservation_seralizer.py
@@ -1,9 +1,12 @@
 # create_reservation.py or serializers.py
 from rest_framework import serializers
 from app.kontres.models.reservation import Reservation
+from app.kontres.serializer.bookable_item_serializer import BookableItemSerializer
 
 
 class ReservationSerializer(serializers.ModelSerializer):
+    bookable_item = BookableItemSerializer(read_only=True)
+
     class Meta:
         model = Reservation
         fields = '__all__'
