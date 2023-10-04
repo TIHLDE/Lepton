150b8efcce1bc14f5e366f13bccdf40b41645eed
Erik Skjellevik <eriskjel@stud.ntnu.no>
2023-10-04 23:19:51 +0200

    fixed bug in reservation_seralizer.py regarding bookableitem id

--- app/kontres/serializer/reservation_seralizer.py
+++ app/kontres/serializer/reservation_seralizer.py
@@ -2,11 +2,14 @@
 from rest_framework import serializers
 from app.kontres.models.reservation import Reservation
 from app.kontres.serializer.bookable_item_serializer import BookableItemSerializer
+from app.kontres.models.bookable_item import BookableItem
 
 
 class ReservationSerializer(serializers.ModelSerializer):
-    bookable_item = BookableItemSerializer(read_only=True)
+    bookable_item = serializers.PrimaryKeyRelatedField(queryset=BookableItem.objects.all())
 
     class Meta:
         model = Reservation
         fields = '__all__'
+
+
