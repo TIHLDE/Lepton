4faaaecec73d471ccff021e6ef521a872845bb14
eriskjel <eriskjel@stud.ntnu.no>
2023-10-04 13:33:04 +0200

    added bookable item serializer

---
+++ app/kontres/serializer/bookable_item_serializer.py
@@ -0,0 +1,10 @@
+# serializers/bookable_item_serializer.py
+
+from rest_framework import serializers
+from ..models.bookable_item import BookableItem
+
+
+class BookableItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookableItem
+        fields = '__all__'
