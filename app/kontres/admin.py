0ba7ffbd4dd0595665b58fc36023c9060b0f0b65
eriskjel <eriskjel@stud.ntnu.no>
2023-10-09 18:48:51 +0200

    added uuid to admin panel

--- app/kontres/admin.py
+++ app/kontres/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-
 from .models.reservation import Reservation
 from .models.bookable_item import BookableItem
 
-admin.site.register(Reservation)
-admin.site.register(BookableItem)
 
+class ReservationAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+class BookableItemAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(BookableItem, BookableItemAdmin)
