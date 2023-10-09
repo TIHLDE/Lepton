0ccaf5b7717c0b9bc57335028fd0976b1ac43627
eriskjel <eriskjel@stud.ntnu.no>
2023-10-09 18:20:18 +0200

    modified urls.py to accommodate uuid and new endpoint

--- app/kontres/urls.py
+++ app/kontres/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from app.kontres.views.reservation import reservation_view
+from app.kontres.views.fetch_bookable_items import bookable_item_view
 
 urlpatterns = [
     path('reservations/', reservation_view, name='reservations'),
-    path('reservations/<int:reservation_id>/', reservation_view, name='single_reservation'),
+    path('reservations/<uuid:reservation_id>/', reservation_view, name='single_reservation'),
+    path('bookable_items/', bookable_item_view, name='bookable_items')
 ]
