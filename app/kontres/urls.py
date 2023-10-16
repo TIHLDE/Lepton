fe4d4c94ba400b285767c7deb37f92f34cf1d0d5
eriskjel <eriskjel@stud.ntnu.no>
2023-10-16 18:02:15 +0200

    initial commit

--- app/kontres/urls.py
+++ app/kontres/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from app.kontres.views.reservation import reservation_view
-from app.kontres.views.fetch_bookable_items import bookable_item_view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app.kontres.views.reservation_new import ReservationViewSet
+from app.kontres.views.fetch_bookable_items_new import BookableItemViewSet  # You'll need to create this class
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'reservations', ReservationViewSet, basename='reservation')
+router.register(r'bookable_items', BookableItemViewSet, basename='bookable_item')  # You'll need to create this class
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('reservations/', reservation_view, name='reservations'),
-    path('reservations/<uuid:reservation_id>/', reservation_view, name='single_reservation'),
-    path('bookable_items/', bookable_item_view, name='bookable_items')
+    path('', include(router.urls)),
 ]
---
+++ app/kontres/views/fetch_bookable_items_new.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from app.kontres.models.bookable_item import BookableItem
+from app.kontres.serializer.bookable_item_serializer import BookableItemSerializer
+
+
+class BookableItemViewSet(viewsets.ViewSet):
+
+    # GET: Retrieve a list of all bookable items
+    def list(self, request):
+        queryset = BookableItem.objects.all()
+        serializer = BookableItemSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # Additional methods (e.g., retrieve, create, update, destroy) could be added here if needed
