2e4c99e99bd074cc7db291176dda615ed21ab230
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-10-21 07:51:43 +0200

    Added models to django admin

--- app/blitzed/admin.py
+++ app/blitzed/admin.py
@@ -1,32 +1,37 @@
 from django.contrib import admin
 
-from app.blitzed.models.session import Session
-from app.blitzed.models.pong_team import PongTeam
+from app.blitzed.models.anonymous_user import AnonymousUser
+from app.blitzed.models.beerpong_tournament import BeerpongTournament
 from app.blitzed.models.pong_match import PongMatch
 from app.blitzed.models.pong_result import PongResult
-from app.blitzed.models.anonymous_user import AnonymousUser
-from app.blitzed.models.beerpong_tournamnet import BeerpongTournament
+from app.blitzed.models.pong_team import PongTeam
+from app.blitzed.models.session import Session
 
 
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(PongTeam)
 class PongTeamAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(PongMatch)
 class PongMatchAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(PongResult)
 class PongResultAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(AnonymousUser)
 class AnonymousUserAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(BeerpongTournament)
 class BeerpongTournamentAdmin(admin.ModelAdmin):
