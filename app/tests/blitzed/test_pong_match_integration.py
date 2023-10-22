afa85e1bc955a7c074483141850c32a7bb466aea
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-10-23 00:30:08 +0200

    Finished tests for pong match

--- app/tests/blitzed/test_pong_match_integration.py
+++ app/tests/blitzed/test_pong_match_integration.py
@@ -31,6 +31,14 @@ def _get_match_post_data(tournament, team1, team2, prev_match):
         "tournament": tournament.id,
     }
 
+def _get_match_put_data(match, tournament):
+    return {
+        "team1": PongTeamFactory(tournament=tournament).id,
+        "team2": PongTeamFactory(tournament=tournament).id,
+        "prev_match": PongMatchFactory(tournament=tournament).id,
+        "tournament": tournament.id,
+    }
+
 
 @pytest.mark.django_db
 def test_that_a_pong_match_can_be_created_without_a_prev_match(
@@ -61,3 +69,33 @@ def test_that_a_pong_match_can_be_created_with_a_prev_match(
     response = default_client.post(url, data)
 
     assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_that_a_pong_match_can_be_updated(
+    default_client, beerpong_tournament
+):
+    """A pong match should be able to be uodated for a tournament"""
+    match = PongMatchFactory(tournament=beerpong_tournament)
+
+    url = _get_detailed_match_url(match)
+    data = _get_match_put_data(match, beerpong_tournament)
+    response = default_client.put(url, data)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_that_a_pong_match_can_be_deleted(
+    default_client, beerpong_tournament
+):
+    """A pong match should be able to be deleted from a tournament"""
+    match = PongMatchFactory(tournament=beerpong_tournament)
+    old_match_count = beerpong_tournament.matches.count()
+
+    url = _get_detailed_match_url(match)
+    response = default_client.delete(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    beerpong_tournament.refresh_from_db()
+    assert beerpong_tournament.matches.count() < old_match_count
