69936d7170d00566de8af91998e681ad91a5d542
eriskjel <eriskjel@stud.ntnu.no>
2023-10-25 11:18:45 +0200

    fixed imports

--- app/kontres/models/bookable_item.py
+++ app/kontres/models/bookable_item.py
@@ -1,11 +1,10 @@
+from django.db import models
+from app.util.models import BaseModel
+from app.common.permissions import BasePermissionModel
 import uuid
 
-from django.db import models
 
-from app.util.models import BaseModel
-
-
-class BookableItem(BaseModel):
+class BookableItem(BaseModel, BasePermissionModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=20)
     description = models.TextField(blank=True)
--- app/kontres/serializer/bookable_item_serializer.py
+++ app/kontres/serializer/bookable_item_serializer.py
@@ -1,8 +1,5 @@
-# serializers/bookable_item_serializer.py
-
 from rest_framework import serializers
-
-from kontres.models.bookable_item import BookableItem
+from app.kontres.models.bookable_item import BookableItem
 
 
 class BookableItemSerializer(serializers.ModelSerializer):
