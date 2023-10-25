d997f39eccd1d7914eb97bb269ca2ac36a30f441
eriskjel <eriskjel@stud.ntnu.no>
2023-10-25 15:24:36 +0200

    removed comments

--- app/kontres/urls.py
+++ app/kontres/urls.py
@@ -4,12 +4,10 @@ from rest_framework.routers import DefaultRouter
 from app.kontres.views.bookable_item import BookableItemViewSet
 from app.kontres.views.reservation import ReservationViewSet
 
-# Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r"reservations", ReservationViewSet, basename="reservation")
 router.register(r"bookable_items", BookableItemViewSet, basename="bookable_item")
 
-# The API URLs are now determined automatically by the router.
 urlpatterns = [
     path("", include(router.urls)),
 ]
