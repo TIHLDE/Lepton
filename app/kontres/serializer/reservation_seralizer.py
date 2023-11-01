58d4f1bf3b5118041bfa647efda309c2aa18be6e
eriskjel <eriskjel@stud.ntnu.no>
2023-11-01 15:44:05 +0100

    added extensive validation logic to prevent overlapping reservations etc

--- app/kontres/serializer/reservation_seralizer.py
+++ app/kontres/serializer/reservation_seralizer.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
+from django.utils import timezone
 from rest_framework import serializers
 
 from app.kontres.models.bookable_item import BookableItem
-from app.kontres.models.reservation import Reservation
+from app.kontres.models.reservation import Reservation, ReservationStateEnum
 
 
 class ReservationSerializer(serializers.ModelSerializer):
@@ -13,11 +15,63 @@ class ReservationSerializer(serializers.ModelSerializer):
         model = Reservation
         fields = "__all__"
 
-    def validate_state(self, value):
-        # If the instance exists and the state is being modified
-        if self.instance and self.instance.state != value:
-            if not self.context["request"].user.is_HS_or_Index_member:
+    def validate_start_time(self, start_time):
+        if start_time < timezone.now():
+            raise serializers.ValidationError("The start time cannot be in the past.")
+        return start_time
+
+    def validate(self, data):
+
+        # Extract the bookable_item, start_time, and end_time, accounting for the possibility they may not be provided
+        bookable_item = data.get(
+            "bookable_item", self.instance.bookable_item if self.instance else None
+        )
+
+        # Validate the state change permission
+        if "state" in data:
+            # Get the list of valid state values from ReservationStateEnum
+            valid_states = [state.value for state in ReservationStateEnum]
+
+            # Validate the state change permission
+            if "state" in data and data["state"] not in valid_states:
                 raise serializers.ValidationError(
-                    "You cannot change the state of the reservation."
+                    {"state": "Invalid state for reservation."}
                 )
-        return value
+
+            if self.instance and data["state"] != self.instance.state:
+                user = self.context["request"].user
+                if not (user and user.is_authenticated and user.is_HS_or_Index_member):
+                    raise serializers.ValidationError(
+                        {
+                            "state": "You do not have permission to change the state of the reservation."
+                        }
+                    )
+
+        # Validate that the end time is after the start time
+        start_time = data.get(
+            "start_time", self.instance.start_time if self.instance else None
+        )
+        end_time = data.get(
+            "end_time", self.instance.end_time if self.instance else None
+        )
+        if start_time and end_time and end_time <= start_time:
+            raise serializers.ValidationError("end_time must be after start_time")
+
+        # Check for overlapping reservations only if necessary fields are present
+        if bookable_item and start_time and end_time:
+            # Build the query for overlapping reservations
+            overlapping_reservations_query = Q(
+                bookable_item=bookable_item,
+                end_time__gt=start_time,
+                start_time__lt=end_time,
+            )
+            # Exclude the current instance if updating
+            if self.instance:
+                overlapping_reservations_query &= ~Q(pk=self.instance.pk)
+            # Check for overlapping reservations
+            if Reservation.objects.filter(overlapping_reservations_query).exists():
+                raise serializers.ValidationError(
+                    "There is an overlapping reservation for the selected time frame."
+                )
+
+        return data
