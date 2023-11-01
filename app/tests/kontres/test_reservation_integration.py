6d35e4c9af7ea39b0cf10034043adfc4facedf77
eriskjel <eriskjel@stud.ntnu.no>
2023-11-01 15:43:25 +0100

    fixed old tests to accomodate new permission system, as well as added new ones

--- app/tests/kontres/test_reservation_integration.py
+++ app/tests/kontres/test_reservation_integration.py
@@ -1,8 +1,12 @@
+from datetime import timedelta
+
+from django.utils import timezone
 from rest_framework import status
 
 import pytest
 
 from app.common.enums import AdminGroup
+from app.kontres.enums import ReservationStateEnum
 from app.kontres.factories.bookable_item_factory import BookableItemFactory
 from app.kontres.factories.reservation_factory import ReservationFactory
 from app.kontres.models.bookable_item import BookableItem
@@ -11,7 +15,28 @@ from app.util.test_utils import get_api_client
 
 
 @pytest.mark.django_db
-def test_user_can_create_reservation(user, bookable_item):
+def test_member_can_create_reservation(member, bookable_item):
+    client = get_api_client(user=member)
+
+    response = client.post(
+        "/kontres/reservations/",
+        {
+            "author": member.user_id,
+            "bookable_item": bookable_item.id,
+            "start_time": "2030-10-10T10:00:00Z",
+            "end_time": "2030-10-10T11:00:00Z",
+        },
+        format="json",
+    )
+
+    assert response.status_code == 201
+    assert response.data["author"] == member.user_id
+    assert response.data["bookable_item"] == bookable_item.id
+    assert response.data["state"] == "PENDING"
+
+
+@pytest.mark.django_db
+def test_non_tihlde_cannot_create_reservation(user, bookable_item):
     client = get_api_client(user=user)
 
     response = client.post(
@@ -25,28 +50,55 @@ def test_user_can_create_reservation(user, bookable_item):
         format="json",
     )
 
-    assert response.status_code == 201
-    assert response.data["author"] == user.user_id
-    assert response.data["bookable_item"] == bookable_item.id
-    assert response.data["state"] == "PENDING"
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_creating_reservation_with_past_start_time(member, bookable_item):
+    client = get_api_client(user=member)
+    past_time = timezone.now() - timezone.timedelta(days=1)
+    response = client.post(
+        "/kontres/reservations/",
+        {
+            "author": member.user_id,
+            "bookable_item": bookable_item.id,
+            "start_time": past_time,
+            "end_time": timezone.now(),
+        },
+        format="json",
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_member_deleting_own_reservation(member, reservation):
+    client = get_api_client(user=member)
+    response = client.delete(f"/kontres/reservations/{reservation.id}/", format="json")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
 @pytest.mark.django_db
 def test_user_cannot_create_confirmed_reservation(bookable_item, member):
     client = get_api_client(user=member)
 
+    # Set start_time to one hour from the current time
+    start_time = timezone.now() + timedelta(hours=1)
+    # Set end_time to two hours from the current time
+    end_time = timezone.now() + timedelta(hours=2)
+
     response = client.post(
         "/kontres/reservations/",
         {
             "author": member.user_id,
             "bookable_item": bookable_item.id,
-            "start_time": "2023-10-10T10:00:00Z",
-            "end_time": "2023-10-10T11:00:00Z",
+            # Format start_time and end_time to ISO format for the POST request
+            "start_time": start_time.isoformat(),
+            "end_time": end_time.isoformat(),
             "state": "CONFIRMED",
         },
         format="json",
     )
-
+    assert response.status_code == status.HTTP_201_CREATED
     assert response.data["state"] == "PENDING"
 
 
@@ -67,12 +119,12 @@ def test_user_cannot_create_reservation_without_author(member, bookable_item):
 
 
 @pytest.mark.django_db
-def test_user_cannot_create_reservation_with_invalid_date_format(user, bookable_item):
-    client = get_api_client(user=user)
+def test_user_cannot_create_reservation_with_invalid_date_format(member, bookable_item):
+    client = get_api_client(user=member)
     response = client.post(
         "/kontres/reservations/",
         {
-            "author": user.user_id,
+            "author": member.user_id,
             "bookable_item_id": bookable_item.id,
             "start_time": "invalid_date_format",
             "end_time": "2023-10-10T11:00:00Z",
@@ -84,18 +136,60 @@ def test_user_cannot_create_reservation_with_invalid_date_format(user, bookable_
 
 
 @pytest.mark.django_db
-def test_admin_can_edit_reservation(reservation, user):
-    client = get_api_client(user=user, group_name=AdminGroup.INDEX)
+def test_admin_can_edit_reservation_to_confirmed(reservation, admin_user):
+    client = get_api_client(user=admin_user)
 
-    reservation_id = str(reservation.id)
     response = client.put(
-        f"/kontres/reservations/{reservation_id}/",
+        f"/kontres/reservations/{reservation.id}/",
         {"state": "CONFIRMED"},
         format="json",
     )
 
     assert response.status_code == 200
     assert response.data["state"] == "CONFIRMED"
+
+
+@pytest.mark.django_db
+def test_admin_can_edit_reservation_to_cancelled(reservation, admin_user):
+    client = get_api_client(user=admin_user)
+
+    response = client.put(
+        f"/kontres/reservations/{reservation.id}/",
+        {"state": "CANCELLED"},
+        format="json",
+    )
+
+    assert response.status_code == 200
+    assert response.data["state"] == "CANCELLED"
+
+
+@pytest.mark.django_db
+def test_updating_reservation_with_valid_times(member, reservation):
+    client = get_api_client(user=member)
+
+    # Set the start_time to one hour from the current time
+    start_time = timezone.now() + timedelta(hours=1)
+    # Set the end_time to two hours from the current time
+    end_time = timezone.now() + timedelta(hours=2)
+
+    response = client.put(
+        f"/kontres/reservations/{reservation.id}/",
+        {
+            # Format start_time and end_time to ISO format for the PUT request
+            "start_time": start_time.isoformat(),
+            "end_time": end_time.isoformat(),
+        },
+        format="json",
+    )
+    assert response.status_code == status.HTTP_200_OK
+    # Convert the times from the response to datetime objects for comparison
+    response_start_time = timezone.datetime.fromisoformat(
+        response.data["start_time"].rstrip("Z")
+    ).replace(tzinfo=timezone.utc)
+    response_end_time = timezone.datetime.fromisoformat(
+        response.data["end_time"].rstrip("Z")
+    ).replace(tzinfo=timezone.utc)
+    assert response_end_time > response_start_time
 
 
 @pytest.mark.django_db
@@ -162,7 +256,7 @@ def test_user_can_fetch_bookable_items_when_none_exist(member):
     client = get_api_client(user=member)
     response = client.get("/kontres/bookable_items/", format="json")
 
-    assert response.status_code == 403
+    assert response.status_code == 200, response
 
 
 @pytest.mark.django_db
@@ -190,11 +284,136 @@ def test_user_cannot_fetch_nonexistent_reservation(member):
 
 
 @pytest.mark.django_db
-def test_admin_can_delete_any_reservation(user, reservation):
-    client = get_api_client(user=user, group_name=AdminGroup.INDEX)
+def test_admin_can_delete_any_reservation(admin_user, reservation):
+    client = get_api_client(user=admin_user)
+    response = client.delete(
+        f"/kontres/reservations/{reservation.id}/",
+        format="json",
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+def test_user_cannot_edit_others_reservation(user, reservation):
+    client = get_api_client(user=user)
+    reservation_id = str(reservation.id)
+    response = client.put(
+        f"/kontres/reservations/{reservation_id}/",
+        {"description": "New Description"},
+        format="json",
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_user_cannot_delete_others_reservation(user, reservation):
+    client = get_api_client(user=user)
     reservation_id = str(reservation.id)
     response = client.delete(
         f"/kontres/reservations/{reservation_id}/",
         format="json",
     )
-    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_admin_cannot_set_invalid_reservation_state(member, reservation):
+    client = get_api_client(user=member, group_name=AdminGroup.INDEX)
+    reservation_id = str(reservation.id)
+    response = client.put(
+        f"/kontres/reservations/{reservation_id}/",
+        {"state": "INVALID_STATE"},
+        format="json",
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_member_cannot_set_own_reservation_to_invalid_state(member, reservation):
+    client = get_api_client(user=member)
+    reservation_id = str(reservation.id)
+    response = client.put(
+        f"/kontres/reservations/{reservation_id}/",
+        {"state": "INVALID_STATE"},
+        format="json",
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_user_cannot_create_reservation_with_end_time_before_start_time(
+    member, bookable_item
+):
+    client = get_api_client(user=member)
+    response = client.post(
+        "/kontres/reservations/",
+        {
+            "author": member.user_id,
+            "bookable_item": bookable_item.id,
+            "start_time": "2023-10-10T12:00:00Z",
+            "end_time": "2023-10-10T11:00:00Z",
+        },
+        format="json",
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_user_can_update_own_reservation_details(member, reservation):
+    client = get_api_client(user=member)
+    reservation_id = str(reservation.id)
+    new_description = "Updated Description"
+    response = client.put(
+        f"/kontres/reservations/{reservation_id}/",
+        {"description": new_description},
+        format="json",
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["description"] == new_description
+
+
+@pytest.mark.django_db
+def test_unauthenticated_request_cannot_create_reservation(bookable_item):
+    client = get_api_client()
+    response = client.post(
+        "/kontres/reservations/",
+        {
+            "bookable_item": bookable_item.id,
+            "start_time": "2023-10-10T10:00:00Z",
+            "end_time": "2023-10-10T11:00:00Z",
+        },
+        format="json",
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_creating_overlapping_reservation(member, bookable_item, admin_user):
+    # Create a confirmed reservation using the ReservationFactory
+    existing_confirmed_reservation = ReservationFactory(
+        bookable_item=bookable_item,
+        start_time=timezone.now() + timezone.timedelta(hours=1),
+        end_time=timezone.now() + timezone.timedelta(hours=2),
+        state=ReservationStateEnum.CONFIRMED,  # Set the reservation as confirmed
+    )
+
+    # Now attempt to create an overlapping reservation
+    client = get_api_client(user=member)
+    overlapping_start_time = (
+        existing_confirmed_reservation.start_time + timezone.timedelta(minutes=30)
+    )
+    response = client.post(
+        "/kontres/reservations/",
+        {
+            "author": member.user_id,
+            "bookable_item": bookable_item.id,
+            "start_time": overlapping_start_time,
+            "end_time": existing_confirmed_reservation.end_time
+            + timezone.timedelta(hours=1),
+            "state": ReservationStateEnum.PENDING,  # This is pending, but it shouldn't matter
+        },
+        format="json",
+    )
+
+    # The system should not allow this, as it overlaps with a confirmed reservation
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
