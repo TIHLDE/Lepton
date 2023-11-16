ded42be45e2b6cf2cc74f439f8d0e0de06013a84
Mads Nylund <73914541+MadsNyl@users.noreply.github.com>
2023-11-16 23:20:07 +0100

    Activity (#736)
    * first version
    
    * changed ordering for activities
    
    * filled out categiory enum
    
    * fixed test
    
    * fixed test again

--- app/content/enums.py
+++ app/content/enums.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from enumchoicefield import ChoiceEnum
+
 
 class UserClass(models.IntegerChoices):
     FIRST = 1
@@ -7,3 +9,12 @@ class UserClass(models.IntegerChoices):
     THIRD = 3
     FORTH = 4
     FIFTH = 5
+
+
+class CategoryEnum(ChoiceEnum):
+    ACTIVITY = "Aktivitet"
+    SOSIALT = "Sosialt"
+    BEDPRES = "Bedpres"
+    KURS = "Kurs"
+    ANNET = "Annet"
+    FADDERUKA = "Fadderuka"
