62543dbaa7c0e1472a651355740cc3d8fb2eeb35
Mads Nylund <madsnyl@gmail.com>
2023-12-12 14:50:06 +0100

    format

--- app/content/views/event.py
+++ app/content/views/event.py
@@ -65,7 +65,7 @@ class EventViewSet(BaseViewSet, ActionMixin):
         expired = self.request.query_params.get("expired", "false").lower() == "true"
         activity = self.request.query_params.get("activity", None)
 
-        if activity == None:
+        if activity is None:
             if expired:
                 return self.queryset.filter(end_date__lt=time).order_by("-start_date")
             return self.queryset.filter(end_date__gte=time)
