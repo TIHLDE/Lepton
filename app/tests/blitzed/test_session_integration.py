7f48aa8138cdda8b0f98a3a521d030e24a64886d
1Cezzo <emiljohnzen@gmail.com>
2024-01-15 17:01:41 +0100

    updating test class for session

--- app/tests/blitzed/test_session_integration.py
+++ app/tests/blitzed/test_session_integration.py
@@ -43,7 +43,7 @@ def test_that_a_member_can_create_a_session(member):
 
 
 @pytest.mark.django_db
-def test_create_session_invalid_data(member):
+def test_create_session_specified_creator(member):
     """A member should be able to create a session with a specified creator"""
     url = _get_session_url()
     client = get_api_client(user=member)
@@ -58,6 +58,23 @@ def test_create_session_invalid_data(member):
     assert response.status_code == status.HTTP_201_CREATED
 
     assert Session.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_create_session_as_user():
+    """A user should not be able to create a session"""
+    user = UserFactory()
+
+    url = _get_session_url()
+    client = get_api_client(user=user)
+
+    data = {
+        "start_time": "2023-10-30T12:00:00Z",
+        "end_time": "2023-10-30T18:00:00Z",
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.django_db
@@ -83,6 +100,88 @@ def test_create_wasted_level():
     response = client.post(url, wasted_graph_data)
 
     assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_create_wasted_level_as_user():
+    """ "A non Tihlde user should not be able to create a wasted level"""
+    sample_session = SessionFactory()
+    user = UserFactory()
+
+    url = _get_wasted_level_url()
+    client = get_api_client(user=user)
+
+    wasted_graph_data = {
+        "user": user.user_id,
+        "session": sample_session.id,
+        "blood_alcohol_level": 0.08,
+    }
+
+    response = client.post(url, wasted_graph_data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_create_wasted_level_with_invalid_user():
+    """A member should not be able to create a wasted level with an invalid user"""
+    sample_session = SessionFactory()
+    user = UserFactory()
+    add_user_to_group_with_name(user, Groups.TIHLDE)
+
+    url = _get_wasted_level_url()
+    client = get_api_client(user=user)
+
+    wasted_graph_data = {
+        "user": 999,
+        "session": sample_session.id,
+        "blood_alcohol_level": 0.08,
+    }
+
+    response = client.post(url, wasted_graph_data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_create_wasted_level_with_invalid_session():
+    """A member should not be able to create a wasted level with an invalid session"""
+    user = UserFactory()
+    add_user_to_group_with_name(user, Groups.TIHLDE)
+
+    url = _get_wasted_level_url()
+    client = get_api_client(user=user)
+
+    wasted_graph_data = {
+        "user": user.user_id,
+        "session": 999,
+        "blood_alcohol_level": 0.08,
+    }
+
+    response = client.post(url, wasted_graph_data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_create_wasted_level_with_invalid_blood_alcohol_level():
+    """A member should not be able to create a wasted level with an invalid blood alcohol level"""
+    sample_session = SessionFactory()
+    user = UserFactory()
+    add_user_to_group_with_name(user, Groups.TIHLDE)
+
+    url = _get_wasted_level_url()
+    client = get_api_client(user=user)
+
+    wasted_graph_data = {
+        "user": user.user_id,
+        "session": sample_session.id,
+        "blood_alcohol_level": "abc",
+    }
+
+    response = client.post(url, wasted_graph_data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 @pytest.mark.django_db
@@ -198,6 +297,64 @@ def test_create_session_users_and_wasted_levels():
     assert wasted_level_response_2.status_code == status.HTTP_201_CREATED
 
     assert user.userwastedlevel_set.count() == 1
+
+    assert user2.userwastedlevel_set.count() == 1
+
+
+@pytest.mark.django_db
+def test_create_session_users_multiple_wasted_levels():
+    session = SessionFactory()
+
+    user = UserFactory()
+    user2 = UserFactory()
+    add_user_to_group_with_name(user, Groups.TIHLDE)
+    add_user_to_group_with_name(user2, Groups.TIHLDE)
+
+    client = get_api_client(user)
+
+    session_url = _get_session_url(session.id)
+    session_data = {
+        "users": [user.user_id, user2.user_id],
+        "start_time": "2023-10-30T12:00:00Z",
+        "end_time": "2023-10-30T18:00:00Z",
+    }
+    session_response = client.put(session_url, session_data)
+
+    assert session_response.status_code == status.HTTP_200_OK
+
+    assert session.users.count() == 2
+
+    wasted_level_url = _get_wasted_level_url()
+    wasted_level_data = {
+        "user": user.user_id,
+        "session": session.id,
+        "blood_alcohol_level": 0.08,
+    }
+    wasted_level_response = client.post(wasted_level_url, wasted_level_data)
+
+    assert wasted_level_response.status_code == status.HTTP_201_CREATED
+
+    wasted_level_url = _get_wasted_level_url()
+    wasted_level_data_2 = {
+        "user": user.user_id,
+        "session": session.id,
+        "blood_alcohol_level": 0.18,
+    }
+    wasted_level_response = client.post(wasted_level_url, wasted_level_data_2)
+
+    assert wasted_level_response.status_code == status.HTTP_201_CREATED
+
+    wasted_level_data_3 = {
+        "user": user2.user_id,
+        "session": session.id,
+        "blood_alcohol_level": 0.10,
+    }
+
+    wasted_level_response_2 = client.post(wasted_level_url, wasted_level_data_3)
+
+    assert wasted_level_response_2.status_code == status.HTTP_201_CREATED
+
+    assert user.userwastedlevel_set.count() == 2
 
     assert user2.userwastedlevel_set.count() == 1
 
