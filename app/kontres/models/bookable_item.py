99a319df3a567346a29c8bdf387230152228408e
Erik Skjellevik <eriskjel@stud.ntnu.no>
2024-02-07 14:28:43 +0100

    added class methods to bookable_item model

--- app/kontres/models/bookable_item.py
+++ app/kontres/models/bookable_item.py
@@ -3,7 +3,7 @@ import uuid
 from django.db import models
 
 from app.common.enums import AdminGroup, Groups
-from app.common.permissions import BasePermissionModel
+from app.common.permissions import BasePermissionModel, check_has_access
 from app.util.models import BaseModel
 
 
@@ -13,6 +13,26 @@ class BookableItem(BaseModel, BasePermissionModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=20)
     description = models.TextField(blank=True)
+
+    @classmethod
+    def has_read_permission(cls, request):
+        return check_has_access(cls.read_access, request)
+
+    @classmethod
+    def has_retrieve_permission(cls, request):
+        return check_has_access(cls.read_access, request)
+
+    @classmethod
+    def has_destroy_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+
+    @classmethod
+    def has_create_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+
+    @classmethod
+    def has_update_permission(cls, request):
+        return check_has_access(cls.write_access, request)
 
     def __str__(self):
         return self.name
