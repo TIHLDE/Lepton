2a9284e3e204a1551f4dbed34a5d72d8cdaca803
Mads Nylund <madsnyl@gmail.com>
2024-02-08 19:12:06 +0100

    added permissions on model for category

--- app/content/models/category.py
+++ app/content/models/category.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-from app.common.enums import AdminGroup
-from app.common.permissions import BasePermissionModel
+from app.common.enums import AdminGroup, Groups
+from app.common.permissions import (
+    BasePermissionModel,
+    check_has_access
+)
 from app.util.models import BaseModel
 
 
 class Category(BaseModel, BasePermissionModel):
     write_access = AdminGroup.all()
+    read_access = (Groups.TIHLDE,)
     text = models.CharField(max_length=200, null=True)
 
     class Meta:
@@ -14,3 +18,23 @@ class Category(BaseModel, BasePermissionModel):
 
     def __str__(self):
         return f"{self.text}"
+
+    @classmethod
+    def has_read_permission(cls, request):
+        return check_has_access(cls.read_access, request)
+    
+    @classmethod
+    def has_retrieve_permission(cls, request):
+        return check_has_access(cls.read_access, request)
+    
+    @classmethod
+    def has_create_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+    
+    @classmethod
+    def has_update_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+    
+    @classmethod
+    def has_destroy_permission(cls, request):
+        return check_has_access(cls.write_access, request)
