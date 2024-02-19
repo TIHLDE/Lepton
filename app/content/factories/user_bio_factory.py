6210173f81d21ced8802899c49eaebbe9ee9cc59
Harry Linrui XU <xulr0820@hotmail.com>
2024-02-19 19:07:16 +0100

    Finished testing bio viewset integration + format

--- app/content/factories/user_bio_factory.py
+++ app/content/factories/user_bio_factory.py
@@ -6,11 +6,9 @@ from factory.django import DjangoModelFactory
 from app.content.factories.user_factory import UserFactory
 from app.content.models.user_bio import UserBio
 
+
 class UserBioFactory(DjangoModelFactory):
     class Meta:
         model = UserBio
 
     user = factory.SubFactory(UserFactory)
-
-
-        
