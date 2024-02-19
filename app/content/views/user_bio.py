f42c217b55421922d9599f6ea9653f0beb385584
Harry Linrui XU <xulr0820@hotmail.com>
2024-02-19 19:06:52 +0100

    Use correct serializers in viewset + added destroy method

--- app/content/views/user_bio.py
+++ app/content/views/user_bio.py
@@ -2,28 +2,31 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 
+from app.common.permissions import BasicViewPermission
 from app.common.viewsets import BaseViewSet
 from app.content.models.user_bio import UserBio
-from app.content.serializers.user_bio import UserBioSerializer
+from app.content.serializers.user_bio import (
+    UserBioCreateSerializer,
+    UserBioSerializer,
+    UserBioUpdateSerializer,
+)
 
 
 class UserBioViewset(BaseViewSet):
-    queryset = (
-        UserBio.objects.all()
-    )  # Queryset = all data som tilhører denne klassen (alle objekter i userbio)
+    queryset = UserBio.objects.all()
     serializer_class = UserBioSerializer
+    permission_classes = [BasicViewPermission]
 
     def create(self, request, *args, **kwargs):
+        data = request.data
 
-        # Intiate base serializer from request data
-        serializer = self.get_serializer(data=request.data)
+        serializer = UserBioCreateSerializer(data=data, context={"request": request})
 
         if not serializer.is_valid():
             return Response(
                 {"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
             )
 
-        # Create instance of user_bio
         user_bio = super().perform_create(serializer, user=request.user)
 
         user_bio_serializer = UserBioSerializer(
@@ -33,32 +36,20 @@ class UserBioViewset(BaseViewSet):
         return Response(user_bio_serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
-        print("inside update")
-        if self.get_object() is None: 
-            print("is none")
-
         bio = self.get_object()
-        serializer = UserBioSerializer(
+        serializer = UserBioUpdateSerializer(
             bio, data=request.data, context={"request": request}
         )
-        print("Created serializer")
         if serializer.is_valid():
-            try: 
-                print(serializer.data)
-                bio = super().perform_update(serializer)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except Exception: 
-                print(Exception)
-        
-        print("Invalid")
+            bio = super().perform_update(serializer)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
         return Response(
             {"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
         )
 
-
-# todo
-# create update retrieve
-# test (post, put, get)
-# autentisering - bruker kan kun oppdatere sin egen bio
-# Kun admin kan endre andre bios + test
-# test alt
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        return Response(
+            {"detail": ("Brukerbio ble slettet")}, status=status.HTTP_200_OK
+        )
