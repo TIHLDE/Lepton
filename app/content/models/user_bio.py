b2a9f2b3fb237a6fe8f8e68527c626b1786ff0bb
Harry Linrui XU <xulr0820@hotmail.com>
2024-02-19 19:05:40 +0100

    Added permissions and inherited from BaseModel

--- app/content/models/user_bio.py
+++ app/content/models/user_bio.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+from app.common.enums import Groups
+from app.common.permissions import BasePermissionModel, check_has_access
 from app.content.models.user import User
+from app.util.models import BaseModel
 
 
-class UserBio(models.Model):
+class UserBio(BaseModel, BasePermissionModel):
+    read_access = (Groups.TIHLDE,)
+    write_access = (Groups.TIHLDE,)
 
     description = models.CharField(max_length=50, blank=True, null=True)
 
@@ -12,3 +17,17 @@ class UserBio(models.Model):
     linkedIn_link = models.URLField(max_length=300, blank=True, null=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bio")
+
+    @classmethod
+    def has_update_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+
+    @classmethod
+    def has_destroy_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+
+    def has_object_update_permission(self, request):
+        return self.user == request.user
+
+    def has_object_destroy_permission(self, request):
+        return self.user == request.user
