43ed3ad9c2c5105af14adf3abfa56a58ab765273
Mads Nylund <madsnyl@gmail.com>
2024-02-20 22:26:28 +0100

    added model permissions for Album

--- app/gallery/models/album.py
+++ app/gallery/models/album.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils.text import slugify
 
 from app.common.enums import AdminGroup
-from app.common.permissions import BasePermissionModel
+from app.common.permissions import BasePermissionModel, check_has_access
 from app.content.models.event import Event
 from app.util.models import BaseModel, OptionalImage
 
@@ -24,3 +24,32 @@ class Album(BaseModel, BasePermissionModel, OptionalImage):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    @classmethod
+    def has_create_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+
+    @classmethod
+    def has_destroy_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+
+    @classmethod
+    def has_update_permission(cls, request):
+        return check_has_access(cls.write_access, request)
+
+    @classmethod
+    def has_list_permission(cls, request):
+        return check_has_access(cls.read_access, request)
+
+    @classmethod
+    def has_retrieve_permission(cls, request):
+        return check_has_access(cls.read_access, request)
+
+    def has_object_destroy_permission(self, request):
+        return True
+
+    def has_object_update_permission(self, request):
+        return True
+
+    def has_object_retrieve_permission(self, request):
+        return True
