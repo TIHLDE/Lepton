3e2618a5ac96d7ca169f5f77b402a09f01386666
Erik Skjellevik <eriskjel@stud.ntnu.no>
2024-02-21 20:53:09 +0100

    fixed seralizer complexity by splitting into methods

--- app/kontres/serializer/reservation_seralizer.py
+++ app/kontres/serializer/reservation_seralizer.py
@@ -12,43 +12,45 @@ class ReservationSerializer(serializers.ModelSerializer):
     bookable_item = serializers.PrimaryKeyRelatedField(
         queryset=BookableItem.objects.all()
     )
-    group = serializers.SlugRelatedField(slug_field='slug', queryset=Group.objects.all(), required=False, allow_null=True)
+    group = serializers.SlugRelatedField(
+        slug_field="slug", queryset=Group.objects.all(), required=False, allow_null=True
+    )
 
     class Meta:
         model = Reservation
         fields = "__all__"
 
-    def validate_start_time(self, start_time):
-        if start_time < timezone.now():
-            raise serializers.ValidationError("Start-tiden kan ikke være i fortiden.")
-        return start_time
-
     def validate(self, data):
-
         user = self.context["request"].user
+        group = data.get("group", None)
+        if group:
+            self.validate_group(group)
+        self.validate_state_change(data, user)
+        self.validate_time_and_overlapping(data)
+        return data
 
-        if 'group' in data and self.instance and data['group'] != self.instance.group:
+    def validate_group(self, value):
+        user = self.context["request"].user
+        group = value
 
-            # Allow HS or Index members to change the group regardless of the reservation state
-            # Other users can only change the group if the reservation state is PENDING
-            if not user.is_HS_or_Index_member and self.instance.state != ReservationStateEnum.PENDING:
-                raise serializers.ValidationError({
-                    "group": "Only HS or Index members can change the group of a non-PENDING reservation."
-                })
+        if self.instance and group != self.instance.group:
+            # Assuming your model logic and permissions are correctly implemented in is_HS_or_Index_member
+            if (
+                not user.is_HS_or_Index_member
+                and self.instance.state != ReservationStateEnum.PENDING
+            ):
+                raise serializers.ValidationError(
+                    "Du har ikke tilgang til å endre gruppen til denne reservasjonsforespørselen."
+                )
 
-        if 'group' in data:
-            group = data['group']
-            if not user.is_member_of(group):
-                raise serializers.ValidationError({
-                    "group": f"Du er ikke medlem av {group.slug} og kan dermed ikke legge inn "
-                             "bestilling på deres vegne."
-                })
+        if group and not user.is_member_of(group):
+            raise serializers.ValidationError(
+                f"Du er ikke medlem av {group.slug} og kan dermed ikke legge inn bestilling på deres vegne."
+            )
 
-        # Extract the bookable_item, start_time, and end_time, accounting for the possibility they may not be provided
-        bookable_item = data.get(
-            "bookable_item", self.instance.bookable_item if self.instance else None
-        )
+        return group
 
+    def validate_state_change(self, data, user):
         # Validate the state change permission
         if "state" in data:
             if self.instance and data["state"] != self.instance.state:
@@ -58,17 +60,25 @@ class ReservationSerializer(serializers.ModelSerializer):
                             "state": "Du har ikke rettigheter til å endre reservasjonsstatusen."
                         }
                     )
+        pass
+
+    def validate_time_and_overlapping(self, data):
 
         # Validate that the end time is after the start time
         start_time = data.get(
             "start_time", self.instance.start_time if self.instance else None
         )
+        if start_time < timezone.now():
+            raise serializers.ValidationError("Start-tiden kan ikke være i fortiden.")
         end_time = data.get(
             "end_time", self.instance.end_time if self.instance else None
         )
         if start_time and end_time and end_time <= start_time:
             raise serializers.ValidationError("Slutt-tid må være etter start-tid")
-
+        # Extract the bookable_item, start_time, and end_time, accounting for the possibility they may not be provided
+        bookable_item = data.get(
+            "bookable_item", self.instance.bookable_item if self.instance else None
+        )
         # Check for overlapping reservations only if necessary fields are present
         if bookable_item and start_time and end_time:
             # Build the query for overlapping reservations
@@ -85,5 +95,4 @@ class ReservationSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(
                     "Det er en reservasjonsoverlapp for det gitte tidsrommet."
                 )
-
-        return data
+        pass
