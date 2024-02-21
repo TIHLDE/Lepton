421ad7d7657687a8b0e457a793a7adcbf136c35c
Mads Nylund <madsnyl@gmail.com>
2024-02-21 17:10:42 +0100

    added viewset and serializer for logentry. Also remove date_hierachy in admin register for LogEntry

--- app/content/factories/__init__.py
+++ app/content/factories/__init__.py
@@ -10,3 +10,4 @@ from app.content.factories.strike_factory import StrikeFactory
 from app.content.factories.toddel_factory import ToddelFactory
 from app.content.factories.priority_pool_factory import PriorityPoolFactory
 from app.content.factories.qr_code_factory import QRCodeFactory
+from app.content.factories.logentry_factory import LogEntryFactory
