f4656667ffae25770dce4127e8f36c2d38c78968
Erik Skjellevik <eriskjel@stud.ntnu.no>
2024-02-27 11:56:31 +0100

    added destroy endpoint for bookable item to account for cascading

--- app/kontres/views/bookable_item.py
+++ app/kontres/views/bookable_item.py
@@ -1,3 +1,7 @@
+from django.db.models import ProtectedError
+from rest_framework import status
+from rest_framework.response import Response
+
 from app.common.permissions import BasicViewPermission
 from app.common.viewsets import BaseViewSet
 from app.kontres.models.bookable_item import BookableItem
@@ -10,3 +14,19 @@ class BookableItemViewSet(BaseViewSet):
     queryset = BookableItem.objects.all()
     serializer_class = BookableItemSerializer
     permission_classes = [BasicViewPermission]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        try:
+            self.perform_destroy(instance)
+        except ProtectedError:
+            return Response(
+                {
+                    "detail": "Cannot delete a bookable item that is part of an existing reservation."
+                },
+                status=status.HTTP_409_CONFLICT,
+            )
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
