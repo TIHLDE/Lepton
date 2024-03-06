b3d3456a018b1987aa01a3e9b4a0c4b33a38ddb4
Erik Skjellevik <98759397+eriskjel@users.noreply.github.com>
2024-03-06 08:11:59 +0100

    fix(kontres)/added basic viewset to bookable item (#773)

--- app/kontres/models/bookable_item.py
+++ app/kontres/models/bookable_item.py
@@ -13,6 +13,7 @@ class BookableItem(BaseModel, BasePermissionModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=20)
     description = models.TextField(blank=True)
+    allows_alcohol = models.BooleanField(default=False)
 
     @classmethod
     def has_read_permission(cls, request):
@@ -33,6 +34,15 @@ class BookableItem(BaseModel, BasePermissionModel):
     @classmethod
     def has_update_permission(cls, request):
         return check_has_access(cls.write_access, request)
+
+    def has_object_destroy_permission(self, request):
+        return self.check_has_admin_permission(request)
+
+    def has_object_update_permission(self, request):
+        return self.check_has_admin_permission(request)
+
+    def check_has_admin_permission(self, request):
+        return check_has_access([AdminGroup.INDEX, AdminGroup.HS], request)
 
     def __str__(self):
         return self.name
