47fd6d9d2c10923c5077f10bcd0e30077bb3cf9a
Erik Skjellevik <eriskjel@stud.ntnu.no>
2024-03-18 15:14:55 +0100

    removed destroy

--- app/kontres/views/bookable_item.py
+++ app/kontres/views/bookable_item.py
@@ -1,21 +1,12 @@
-from rest_framework import status
-
 from app.common.permissions import BasicViewPermission
 from app.common.viewsets import BaseViewSet
 from app.kontres.models.bookable_item import BookableItem
 from app.kontres.serializer.bookable_item_serializer import (
     BookableItemSerializer,
 )
-from rest_framework.response import Response
 
 
 class BookableItemViewSet(BaseViewSet):
     queryset = BookableItem.objects.all()
     serializer_class = BookableItemSerializer
     permission_classes = [BasicViewPermission]
-
-    def destroy(self, request, *args, **kwargs):
-        super().destroy(self, request, *args, **kwargs)
-        return Response(status=status.HTTP_200_OK)
-
-
