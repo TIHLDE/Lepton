38c7287f6e23d5b6573e1e4d4ccf342e823c854c
eriskjel <eriskjel@stud.ntnu.no>
2024-03-23 13:38:51 +0100

    remove update method for images

--- app/kontres/serializer/bookable_item_serializer.py
+++ app/kontres/serializer/bookable_item_serializer.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 
-from app.common.file_handler import replace_file
 from app.kontres.models.bookable_item import BookableItem
 
 
@@ -8,8 +7,3 @@ class BookableItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookableItem
         fields = "__all__"
-
-    def update(self, instance, validated_data):
-        if hasattr(instance, "image") and "image" in validated_data:
-            replace_file(instance.image, validated_data.get("image", None))
-        return super().update(instance, validated_data)
