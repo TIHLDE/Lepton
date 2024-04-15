87974ced19b4ed50b610158a8d11a433b6ebb89f
Tam Le <tamle2107@hotmail.com>
2024-04-15 20:26:41 +0200

    added id to serializer

--- app/content/serializers/user_bio.py
+++ app/content/serializers/user_bio.py
@@ -5,7 +5,7 @@ from app.content.models.user_bio import UserBio
 class UserBioSerializer(BaseModelSerializer):
     class Meta:
         model = UserBio
-        fields = ["description", "gitHub_link", "linkedIn_link"]
+        fields = ["id", "description", "gitHub_link", "linkedIn_link"]
 
 
 class UserBioCreateSerializer(BaseModelSerializer):
