0f24085a9d115830fa464382742ab4c59787f7a0
Mads Nylund <73914541+MadsNyl@users.noreply.github.com>
2024-04-17 20:43:28 +0200

    fixed bug (#800)

--- app/content/util/registration_utils.py
+++ app/content/util/registration_utils.py
@@ -1,5 +1,12 @@
 from datetime import datetime, timedelta
 
 
-def get_payment_expiredate():
-    return datetime.now() + timedelta(hours=12)
+def get_payment_expiredate(event=None):
+    if not event:
+        return datetime.now() + timedelta(hours=12)
+
+    return datetime.now() + timedelta(
+        hours=event.paid_information.paytime.hour,
+        minutes=event.paid_information.paytime.minute,
+        seconds=event.paid_information.paytime.second,
+    )
