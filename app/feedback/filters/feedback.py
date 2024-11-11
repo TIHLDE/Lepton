3fb533bfc4214e103e1e945833642da39bb5bdd0
Mads Nylund <madsnyl@gmail.com>
2024-11-11 19:58:51 +0100

    format

--- app/feedback/filters/feedback.py
+++ app/feedback/filters/feedback.py
@@ -8,7 +8,6 @@ from app.feedback.models.idea import Idea
 
 
 class FeedbackFilter(filters.FilterSet):
-
     feedback_type = filters.CharFilter(
         method="filter_feedback_type", label="List of feedback type"
     )
@@ -26,13 +25,10 @@ class FeedbackFilter(filters.FilterSet):
     )
 
     def filter_feedback_type(self, queryset, _name, feedback_type):
-
         if feedback_type == "Idea":
             return queryset.filter(Q(instance_of=Idea))
-
         elif feedback_type == "Bug":
             return queryset.filter(Q(instance_of=Bug))
-
         else:
             return queryset
 
