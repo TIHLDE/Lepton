94e5635dade6f7647454dfe6659d1cccd5847028
Johannes Aamot-Skeidsvoll <82368148+Tmpecho@users.noreply.github.com>
2024-11-11 20:02:44 +0100

    Move file upload and deletion to content from files (#928)
    Signed-off-by: Tmpecho <TMPEcho@gmail.com>
    Co-authored-by: Mads Nylund <73914541+MadsNyl@users.noreply.github.com>

--- app/files/urls.py
+++ app/files/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from rest_framework import routers
 
 from app.files.views.file import FileViewSet
-from app.files.views.upload import delete, upload
 from app.files.views.user_gallery import UserGalleryViewSet
 
 router = routers.DefaultRouter()
@@ -11,7 +10,5 @@ router.register("file", FileViewSet, basename="file")
 router.register("user_gallery", UserGalleryViewSet, basename="user_gallery")
 
 urlpatterns = [
-    path("upload/", upload),
-    path("delete-file/<str:container_name>/<str:blob_name>/", delete),
     path("files/", include(router.urls)),
 ]
