6d88724bc32cea29d0d35bc34430003ef2424eb4
Tmpecho <TMPEcho@gmail.com>
2025-09-22 17:07:04 +0200

    Remove unused import from views/feedback.py
    Signed-off-by: Tmpecho <TMPEcho@gmail.com>

--- app/feedback/views/feedback.py
+++ app/feedback/views/feedback.py
@@ -1,4 +1,3 @@
-from django.db.models import Count, Q
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status
 from rest_framework.response import Response
